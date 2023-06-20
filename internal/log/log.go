75cdc6b7d8ce074b67a004511a96db0bce7ca0de
Didier Roche <didier.roche@canonical.com>
2023-06-20 15:47:36 +0200

    Missing logs methods stub
    Those are the stubs until we centralize logs in another repo.
    
    Co-authored-by: Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>

--- internal/log/log.go
+++ internal/log/log.go
@@ -26,6 +26,22 @@ func Debug(ctx context.Context, args ...interface{}) {
 	logrus.Debug(args...)
 }
 
+func Debugf(ctx context.Context, format string, args ...interface{}) {
+	logrus.Debugf(format, args...)
+}
+
+func Info(ctx context.Context, args ...interface{}) {
+	logrus.Info(args...)
+}
+
+func Warningf(ctx context.Context, format string, args ...interface{}) {
+	logrus.Warningf(format, args...)
+}
+
 func Error(ctx context.Context, args ...interface{}) {
 	logrus.Error(args...)
+}
+
+func Infof(ctx context.Context, format string, args ...interface{}) {
+	logrus.Infof(format, args...)
 }
