e2ac590f344f9c4b64112eab5218e97f4e7954aa
Didier Roche <didier.roche@canonical.com>
2023-06-30 09:36:50 +0200

    Move dbus broker as embeeded from Broker
    This will allows us to have some example/mocks brokers in memory, while
    keeping some common logic.
    
    Co-authored-by: Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>

--- internal/brokers/broker.go
+++ internal/brokers/broker.go
@@ -3,64 +3,57 @@ package brokers
 import (
 	"context"
 	"fmt"
+	"hash/fnv"
 
 	"github.com/godbus/dbus/v5"
 	"github.com/ubuntu/authd/internal/log"
 	"github.com/ubuntu/decorate"
-	"gopkg.in/ini.v1"
 )
 
+type brokerer interface {
+	GetAuthenticationModes(ctx context.Context, username, lang string, supportedUiLayouts []map[string]string) (sessionID, encryptionKey string, authenticationModes []map[string]string, err error)
+	SelectAuthenticationMode(ctx context.Context, sessionID, authenticationModeName string) (uiLayoutInfo map[string]string, err error)
+	IsAuthorized(ctx context.Context, sessionID, authenticationData string) (access, infoUser string, err error)
+}
+
 type Broker struct {
+	id            string
 	name          string
 	brandIconPath string
-	dbusObject    dbus.BusObject
-	interfaceName string
+	brokerer
 }
 
 func NewBroker(ctx context.Context, name, configFile string, bus *dbus.Conn) (b Broker, err error) {
-	defer decorate.OnError(&err, "can't create broker from %q", configFile)
+	defer decorate.OnError(&err, "can't create broker %q", name)
 
-	log.Debugf(ctx, "Loading broker configuration %q", configFile)
+	h := fnv.New32a()
+	h.Write([]byte(name))
+	id := h.Sum32()
 
-	cfg, err := ini.Load(configFile)
-	if err != nil {
-		return b, fmt.Errorf("could not read ini configuration for broker %q: %v", name, err)
-	}
-
-	fullName, err := cfg.Section("").GetKey("name")
-	if err != nil {
-		return b, fmt.Errorf("missing field for broker %q: %v", name, err)
-	}
-
-	brandIcon, err := cfg.Section("").GetKey("brand_icon")
-	if err != nil {
-		return b, fmt.Errorf("missing field for broker %q: %v", name, err)
-	}
-
-	n, err := cfg.Section("dbus").GetKey("name")
-	if err != nil {
-		return b, fmt.Errorf("missing field for broker %q: %v", name, err)
-	}
-
-	o, err := cfg.Section("dbus").GetKey("object")
-	if err != nil {
-		return b, fmt.Errorf("missing field for broker %q: %v", name, err)
-	}
-
-	interfaceName, err := cfg.Section("dbus").GetKey("interface")
-	if err != nil {
-		return b, fmt.Errorf("missing field for broker %q: %v", name, err)
+	var broker brokerer
+	var fullName, brandIcon string
+	log.Debugf(ctx, "Loading broker %q", name)
+	if configFile != "" {
+		broker, fullName, brandIcon, err = newDbusBroker(ctx, bus, configFile)
+		if err != nil {
+			return Broker{}, err
+		}
+	} else {
+		broker, fullName, brandIcon, err = newExampleBroker(name)
+		if err != nil {
+			return Broker{}, err
+		}
 	}
 
 	return Broker{
-		name:          fullName.String(),
-		brandIconPath: brandIcon.String(),
-		dbusObject:    bus.Object(n.String(), dbus.ObjectPath(o.String())),
-		interfaceName: interfaceName.String(),
+		id:            fmt.Sprint(id),
+		name:          fullName,
+		brandIconPath: brandIcon,
+		brokerer:      broker,
 	}, nil
 }
 
 // Info is the textual and graphical representation of a broker information.
-func (b Broker) Info() (name, brandIconPath string) {
-	return b.name, b.brandIconPath
+func (b Broker) Info() (id, name, brandIconPath string) {
+	return b.id, b.name, b.brandIconPath
 }
---
+++ internal/brokers/dbusbroker.go
@@ -0,0 +1,63 @@
+package brokers
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/godbus/dbus/v5"
+	"github.com/ubuntu/authd/internal/log"
+	"github.com/ubuntu/decorate"
+	"gopkg.in/ini.v1"
+)
+
+type dbusBroker struct {
+	dbusObject    dbus.BusObject
+	interfaceName string
+}
+
+// newDbusBroker returns a dbus broker and broker attributes from its configuration file.
+func newDbusBroker(ctx context.Context, bus *dbus.Conn, configFile string) (b dbusBroker, fullName, brandIcon string, err error) {
+	defer decorate.OnError(&err, "dbus broker from configuration file: %q", configFile)
+
+	log.Debugf(ctx, "Dbus broker configuration at %q", configFile)
+
+	cfg, err := ini.Load(configFile)
+	if err != nil {
+		return b, "", "", fmt.Errorf("could not read ini configuration for broker %v", err)
+	}
+
+	fullNameVal, err := cfg.Section("").GetKey("name")
+	if err != nil {
+		return b, "", "", fmt.Errorf("missing field for broker: %v", err)
+	}
+
+	brandIconVal, err := cfg.Section("").GetKey("brand_icon")
+	if err != nil {
+		return b, "", "", fmt.Errorf("missing field for broker: %v", err)
+	}
+
+	dbusName, err := cfg.Section("dbus").GetKey("name")
+	if err != nil {
+		return b, "", "", fmt.Errorf("missing field for broker: %v", err)
+	}
+
+	objectName, err := cfg.Section("dbus").GetKey("object")
+	if err != nil {
+		return b, "", "", fmt.Errorf("missing field for broker: %v", err)
+	}
+
+	interfaceName, err := cfg.Section("dbus").GetKey("interface")
+	if err != nil {
+		return b, "", "", fmt.Errorf("missing field for broker: %v", err)
+	}
+
+	return dbusBroker{
+		dbusObject:    bus.Object(dbusName.String(), dbus.ObjectPath(objectName.String())),
+		interfaceName: interfaceName.String(),
+	}, fullNameVal.String(), brandIconVal.String(), nil
+}
+
+// To be implemented.
+func (b dbusBroker) GetAuthenticationModes(ctx context.Context, username, lang string, supportedUiLayouts []map[string]string) (sessionID, encryptionKey string, authenticationModes []map[string]string, err error)
+func (b dbusBroker) SelectAuthenticationMode(ctx context.Context, sessionID, authenticationModeName string) (uiLayoutInfo map[string]string, err error)
+func (b dbusBroker) IsAuthorized(ctx context.Context, sessionID, authenticationData string) (access, infoUser string, err error)
