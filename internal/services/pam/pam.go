190a85666c5d49d60259d24f1ab3b4da5ddcd3b6
Didier Roche <didier.roche@canonical.com>
2023-07-03 14:48:00 +0200

    Adapt PAM grpc module to new API
    Co-authored-by: Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>

--- internal/services/pam/pam.go
+++ internal/services/pam/pam.go
@@ -4,13 +4,13 @@ package pam
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
-	"strings"
-	"time"
 
 	"github.com/ubuntu/authd"
 	"github.com/ubuntu/authd/internal/brokers"
 	"github.com/ubuntu/authd/internal/log"
+	"github.com/ubuntu/decorate"
 	"golang.org/x/exp/slices"
 )
 
@@ -33,304 +33,248 @@ func NewService(ctx context.Context, brokerManager *brokers.Manager) Service {
 }
 
 // GetBrokersInfo returns the list of all brokers with their details.
-func (s Service) GetBrokersInfo(context.Context, *authd.Empty) (*authd.BrokersInfo, error) {
-	var r authd.BrokersInfo
+// It can return the previous broker set for a given user, if any and if provided.
+func (s Service) AvailableBrokers(ctx context.Context, req *authd.ABRequest) (*authd.ABResponse, error) {
+	var r authd.ABResponse
 
-	brandIcon1, brandIcon2 := "/my/image/broker1", "/my/image/broker2"
-	r.BrokersInfos = append(r.BrokersInfos, &authd.BrokersInfo_BrokerInfo{
-		Name:      "warthogs",
-		BrandIcon: &brandIcon1,
-	}, &authd.BrokersInfo_BrokerInfo{
-		Name:      "no named",
-		BrandIcon: &brandIcon2,
-	})
-
-	/*for _, b := range s.brokerManager.AvailableBrokers() {
-		name, brandIcon := b.Info()
-		r.BrokersInfos = append(r.BrokersInfos, &authd.BrokersInfo_BrokerInfo{
-			Name:      name,
-			BrandIcon: brandIcon,
+	for _, b := range s.brokerManager.AvailableBrokers() {
+		r.BrokersInfos = append(r.BrokersInfos, &authd.ABResponse_BrokerInfo{
+			Id:        b.ID,
+			Name:      b.Name,
+			BrandIcon: &b.BrandIconPath,
 		})
-	}*/
+	}
+
+	if req.GetUserName() != "" {
+		b := s.brokerManager.BrokerForUser(req.GetUserName())
+		if b != nil {
+			r.PreviousBroker = &b.ID
+		}
+	}
 
 	return &r, nil
 }
 
-type sessionInfo struct {
-	username     string
-	selectedMode string
-}
+// GetAuthenticationModes fetches the list for the selected broker of authentication mode depending on user name.
+func (s Service) SelectBroker(ctx context.Context, req *authd.SBRequest) (resp *authd.SBResponse, err error) {
+	defer decorate.OnError(&err, "can't start authentication transaction")
 
-var currentSessions = make(map[string]sessionInfo)
+	username := req.GetUsername()
+	brokerID := req.GetBrokerId()
+	lang := req.GetLang()
 
-func (s Service) GetAuthenticationModes(ctx context.Context, req *authd.GetAuthenticationModesRequest) (resp *authd.GetAuthenticationModesResponse, err error) {
-	_ = req.GetBroker()
-	_ = req.GetLang()
-
-	var authModes []*authd.GetAuthenticationModesResponse_AuthenticationMode
-
-	for _, layout := range req.GetSupportedUiLayouts() {
-		switch layout.Type {
-		case "form":
-			if layout.GetEntry() != "" {
-				supportedEntries := strings.Split(strings.TrimPrefix(layout.GetEntry(), "optional:"), ",")
-				if slices.Contains(supportedEntries, "chars_password") {
-					authModes = append(authModes, &authd.GetAuthenticationModesResponse_AuthenticationMode{
-						Name:  "password",
-						Label: "Password authentication",
-					})
-				}
-				if slices.Contains(supportedEntries, "digits") {
-					authModes = append(authModes, &authd.GetAuthenticationModesResponse_AuthenticationMode{
-						Name:  "pincode",
-						Label: "Pin code",
-					})
-				}
-				if slices.Contains(supportedEntries, "chars") && layout.GetWait() != "" {
-					authModes = append(authModes, &authd.GetAuthenticationModesResponse_AuthenticationMode{
-						Name:  "entry_or_wait_for_foogmail.com",
-						Label: "Send URL to foo@gmail.com",
-					})
-				}
-			}
-
-			// The broker could parse the values, that are either true/false
-			if layout.GetWait() != "" {
-				if layout.GetButton() == "optional" {
-					authModes = append(authModes, &authd.GetAuthenticationModesResponse_AuthenticationMode{
-						Name:  "topt_with_button",
-						Label: "Authentication code",
-					})
-				} else {
-					authModes = append(authModes, &authd.GetAuthenticationModesResponse_AuthenticationMode{
-						Name:  "topt",
-						Label: "Authentication code",
-					})
-					authModes = append(authModes, &authd.GetAuthenticationModesResponse_AuthenticationMode{
-						Name:  "phoneack1",
-						Label: "Use your phone +33…",
-					})
-					authModes = append(authModes, &authd.GetAuthenticationModesResponse_AuthenticationMode{
-						Name:  "phoneack2",
-						Label: "Use your phone +1…",
-					})
-				}
-				authModes = append(authModes, &authd.GetAuthenticationModesResponse_AuthenticationMode{
-					Name:  "fidodevice1",
-					Label: "Use your fido device",
-				})
-			}
-
-		case "qrcode":
-			authModes = append(authModes, &authd.GetAuthenticationModesResponse_AuthenticationMode{
-				Name:  "qrcodewithtypo",
-				Label: "Use a QR code",
-			})
-		case "webview":
-			// This broker does not support webview
-		}
+	if username == "" {
+		return nil, errors.New("no user name provided")
+	}
+	if brokerID == "" {
+		return nil, errors.New("no broker selected")
+	}
+	if lang == "" {
+		lang = "C"
 	}
 
-	// TODO authModes: sort in preference order
+	broker, err := s.brokerManager.GetBroker(brokerID)
+	if err != nil {
+		return nil, fmt.Errorf("invalid broker: %v", err)
+	}
+	s.brokerManager.SetDefaultBrokerForUser(username, broker)
 
-	generatedSessionID := "my-random-id"
-	currentSessions[generatedSessionID] = sessionInfo{username: req.GetUsername()}
+	var layouts []map[string]string
+	for _, l := range req.GetSupportedUiLayouts() {
+		layout, err := uiLayoutToMap(l)
+		if err != nil {
+			return nil, err
+		}
+		layouts = append(layouts, layout)
+	}
 
-	return &authd.GetAuthenticationModesResponse{
-		SessionId:           "my-random-id",
-		EncryptionKey:       "my secret public encryption key",
+	sessionID, encryptionKey, authenticationModes, err := broker.GetAuthenticationModes(ctx, username, lang, layouts)
+	if err != nil {
+		return nil, err
+	}
+
+	var authModes []*authd.SBResponse_AuthenticationMode
+	for _, a := range authenticationModes {
+		name := a["name"]
+		if name == "" {
+			return nil, fmt.Errorf("invalid authentication mode, missing name key: %v", a)
+		}
+		label := a["label"]
+		if label == "" {
+			return nil, fmt.Errorf("invalid authentication mode, missing name key: %v", a)
+		}
+		authModes = append(authModes, &authd.SBResponse_AuthenticationMode{
+			Name:  name,
+			Label: label,
+		})
+	}
+
+	if len(authModes) < 1 {
+		return &authd.SBResponse{
+			SessionId:           "",
+			EncryptionKey:       "",
+			AuthenticationModes: authModes,
+		}, nil
+	}
+
+	// Memorizes selected broker for this session.
+	s.brokerManager.SetBrokerForSessionID(sessionID, broker)
+
+	return &authd.SBResponse{
+		SessionId:           sessionID,
+		EncryptionKey:       encryptionKey,
 		AuthenticationModes: authModes,
 	}, nil
 }
 
+// SelectAuthenticationMode set given authentication mode as selected for this sessionID to the broker.
 func (s Service) SelectAuthenticationMode(ctx context.Context, req *authd.SAMRequest) (resp *authd.SAMResponse, err error) {
-	// Ensure session ID is an active one.
+	defer decorate.OnError(&err, "can't select authentication mode")
 
-	authMode := req.GetAuthenticationModeName()
-	fmt.Println("SELECT:", authMode, req.SessionId)
+	sessionID := req.GetSessionId()
+	authenticationModeName := req.GetAuthenticationModeName()
 
-	// Store selected mode
-	currentSessionInfo := currentSessions[req.SessionId]
-	currentSessionInfo.selectedMode = authMode
-	currentSessions[req.SessionId] = currentSessionInfo
+	if sessionID == "" {
+		return nil, errors.New("no session ID provided")
+	}
+	if authenticationModeName == "" {
+		return nil, errors.New("no authentication mode provided")
+	}
 
-	trueStr := "true"
-	chars := "chars"
-	charsPassword := "chars_password"
-	digits := "digits"
+	broker, err := s.brokerManager.BrokerForSessionID(sessionID)
+	if err != nil {
+		return nil, err
+	}
 
-	// populate UI options based on selected authentication mode
-	var layoutInfo authd.UILayout
-	switch authMode {
-	case "password":
-		promptMsg := "Gimme your password:"
-		layoutInfo = authd.UILayout{
-			Type:  "form",
-			Label: &promptMsg,
-			Entry: &charsPassword,
-		}
-	case "pincode":
-		promptMsg := "Enter your pin code:"
-		layoutInfo = authd.UILayout{
-			Type:  "form",
-			Label: &promptMsg,
-			Entry: &digits,
-		}
-	case "entry_or_wait_for_foogmail.com":
-		promptMsg := "Click on the link received at foo@gmail.com or enter the code:"
-		layoutInfo = authd.UILayout{
-			Type:  "form",
-			Label: &promptMsg,
-			Entry: &chars,
-			Wait:  &trueStr,
-		}
-	case "topt_with_button":
-		promptMsg := "Enter your one time credential:"
-		buttonMsg := "Resend sms"
-		layoutInfo = authd.UILayout{
-			Type:   "form",
-			Label:  &promptMsg,
-			Button: &buttonMsg,
-			Wait:   &trueStr,
-		}
-	case "topt":
-		promptMsg := "Enter your one time credential:"
-		layoutInfo = authd.UILayout{
-			Type:  "form",
-			Label: &promptMsg,
-			Wait:  &trueStr,
-		}
-	case "phoneack1":
-		promptMsg := "Unlock your phone +33… or accept request on web interface:"
-		layoutInfo = authd.UILayout{
-			Type:  "form",
-			Label: &promptMsg,
-			Wait:  &trueStr,
-		}
-	case "phoneack2":
-		promptMsg := "Unlock your phone +1… or accept request on web interface:"
-		layoutInfo = authd.UILayout{
-			Type:  "form",
-			Label: &promptMsg,
-			Wait:  &trueStr,
-		}
-	case "fidodevice1":
-		promptMsg := "Plug your fido device and press with your thumb"
-		layoutInfo = authd.UILayout{
-			Type:  "form",
-			Label: &promptMsg,
-			Wait:  &trueStr,
-		}
-	case "qrcodewithtypo":
-		promptMsg := "Enter the following code after flashing the address: 1337"
-		url := "https://ubuntu.com"
-		layoutInfo = authd.UILayout{
-			Type:    "qrcode",
-			Label:   &promptMsg,
-			Content: &url,
-			Wait:    &trueStr,
-		}
+	uiLayoutInfo, err := broker.SelectAuthenticationMode(ctx, sessionID, authenticationModeName)
+	if err != nil {
+		return nil, err
+	}
+
+	layoutInfo, err := validateMapToUILayout(uiLayoutInfo)
+	if err != nil {
+		return nil, err
 	}
 
 	return &authd.SAMResponse{
-		UiLayoutInfo: &layoutInfo,
+		UiLayoutInfo: layoutInfo,
 	}, nil
 }
 
 // IsAuthorized returns broker answer to authorization request.
 func (s Service) IsAuthorized(ctx context.Context, req *authd.IARequest) (resp *authd.IAResponse, err error) {
-	sessionInfo := currentSessions[req.SessionId]
-	authDeniedResp := &authd.IAResponse{Access: "denied"}
+	defer decorate.OnError(&err, "can't check authorization")
 
-	users := make(map[string]string)
-	users["user1"] = `
-		"uid": "4245874",
-		"name": "My user",
-		"groups": [
-			"group1": {
-				"name": "Group 1",
-				"gid": "3884"
-			},
-			"group2": {
-				"name": "Group 2",
-				"gid": "4884"
-			},
-		]
-	`
-	users["user2"] = `
-		"uid": "3333333",
-		"name": "My secondary user",
-		"groups": [
-			"group2": {
-				"name": "Group 2",
-				"gid": "4884"
-			},
-		]
-	`
+	sessionID := req.GetSessionId()
+	if sessionID == "" {
+		return nil, errors.New("no session ID provided")
+	}
 
-	data, err := authenticationData(req.GetAuthenticationData())
+	broker, err := s.brokerManager.BrokerForSessionID(sessionID)
 	if err != nil {
-		fmt.Println("Error unmarshaling JSON:", err)
-		return authDeniedResp, err
+		return nil, err
 	}
 
-	switch currentSessions[req.SessionId].selectedMode {
-	case "password":
-		if data["challenge"] != "goodpass" {
-			return authDeniedResp, nil
-		}
-
-	case "pincode":
-		if data["challenge"] != "4242" {
-			return authDeniedResp, nil
-		}
-
-	case "entry_or_wait_for_foogmail.com":
-		// do we have a challenge sent or should we just wait?
-		if data["challenge"] != "" {
-			// validate challenge given manually by the user
-			if data["challenge"] != "abcde" {
-				return authDeniedResp, nil
-			}
-		} else {
-			// we are simulating clicking on the url signal received by the broker
-			time.Sleep(10 * time.Second)
-		}
-
-	case "topt_with_button", "topt":
-		if data["challenge"] != "temporary pass" {
-			return authDeniedResp, nil
-		}
-
-	case "phoneack1":
-		// Send notification to phone1 and wait on server signal to return if OK or not
-		time.Sleep(5 * time.Second)
-
-	case "phoneack2":
-		// This one is failing remotely as an example
-		time.Sleep(2 * time.Second)
-		return authDeniedResp, nil
-
-	case "fidodevice1":
-		// simulate direct exchange with the FIDO device
-		time.Sleep(5 * time.Second)
-
-	case "qrcodewithtypo":
-		// Simulate connexion with remote server to check that the correct code was entered
-		time.Sleep(4 * time.Second)
+	access, rawUserInfo, err := broker.IsAuthorized(ctx, sessionID, req.GetAuthenticationData())
+	if err != nil {
+		return nil, err
 	}
 
-	userInfo, exists := users[sessionInfo.username]
-	if !exists {
-		return authDeniedResp, nil
+	// Validate access authorization
+	if !slices.Contains([]string{"access", "denied"}, access) {
+		return nil, fmt.Errorf("invalid access authorization key: %v", access)
 	}
-	// TODO in authd itself: store userinfo here for nss
-	_ = userInfo
 
-	return &authd.IAResponse{Access: "allowed"}, nil
+	var userInfo map[string]string
+	if rawUserInfo != "" {
+		userInfo, err = stringToMap(rawUserInfo)
+		if err != nil {
+			return nil, fmt.Errorf("can't decode user info: %v", err)
+		}
+	}
+
+	return &authd.IAResponse{
+		Access:   access,
+		UserInfo: userInfo,
+	}, nil
 }
 
-func authenticationData(jsonData string) (map[string]string, error) {
+func uiLayoutToMap(layout *authd.UILayout) (mapLayout map[string]string, err error) {
+	if layout.GetType() == "" {
+		return nil, fmt.Errorf("invalid layout option: type is required, got: %v", layout)
+	}
+	r := map[string]string{"type": layout.GetType()}
+	if l := layout.GetLabel(); l != "" {
+		r["label"] = l
+	}
+	if b := layout.GetButton(); b != "" {
+		r["button"] = b
+	}
+	if w := layout.GetWait(); w != "" {
+		r["wait"] = w
+	}
+	if e := layout.GetEntry(); e != "" {
+		r["entry"] = e
+	}
+	if c := layout.GetContent(); c != "" {
+		r["content"] = c
+	}
+	return r, nil
+}
+
+// validateMapToUILayout generates an UILayout from the input map.
+// It validates the required fields for a given type.
+func validateMapToUILayout(layout map[string]string) (r *authd.UILayout, err error) {
+	defer decorate.OnError(&err, "no valid UI layouts metadata")
+
+	typ := layout["type"]
+	label := layout["label"]
+	entry := layout["entry"]
+	button := layout["button"]
+	wait := layout["wait"]
+	content := layout["content"]
+
+	var res authd.UILayout
+	switch typ {
+	case "form":
+		if label == "" {
+			return nil, fmt.Errorf("'label' is required")
+		}
+		if !slices.Contains([]string{"chars", "digits", "chars_password", "digits_password", ""}, entry) {
+			return nil, fmt.Errorf("'entry' does not match allowed values for this type: %v", entry)
+		}
+		if !slices.Contains([]string{"true", "false", ""}, wait) {
+			return nil, fmt.Errorf("'wait' does not match allowed values for this type: %v", wait)
+		}
+		res = authd.UILayout{
+			Type:   typ,
+			Label:  &label,
+			Entry:  &entry,
+			Button: &button,
+			Wait:   &wait,
+		}
+	case "qrcode":
+		if content == "" {
+			return nil, fmt.Errorf("'content' is required")
+		}
+		if !slices.Contains([]string{"true", "false"}, wait) {
+			return nil, fmt.Errorf("'wait' is required and does not match allowed values for this type: %v", wait)
+		}
+		res = authd.UILayout{
+			Type:   typ,
+			Wait:   &wait,
+			Label:  &label,
+			Button: &button,
+		}
+	case "webview":
+	default:
+		return nil, fmt.Errorf("invalid layout option: type is required, got: %v", layout)
+	}
+
+	return &res, nil
+}
+
+func stringToMap(jsonData string) (map[string]string, error) {
 	var data map[string]string
 
 	err := json.Unmarshal([]byte(jsonData), &data)
