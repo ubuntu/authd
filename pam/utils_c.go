e149c97a405a004affc8f31ce2b716bbb04cca13
Marco Trevisan (Treviño) <mail@3v1n0.net>
2023-07-21 04:48:30 +0200

    pam/utils: Add utilities to get module name and do basic PAM conversation

--- pam/utils_c.go
+++ pam/utils_c.go
@@ -20,6 +20,41 @@ char *get_user(pam_handle_t *pamh, const char *prompt) {
     return NULL;
   return strdup(user);
 }
+
+const char *
+get_module_name (pam_handle_t *pamh)
+{
+  const char *module_name;
+
+  if (pam_get_item(pamh, PAM_SERVICE, (const void **) &module_name) != PAM_SUCCESS)
+    return NULL;
+
+  return module_name;
+}
+
+static struct pam_response *
+send_msg (pam_handle_t *pamh, const char *msg, int style)
+{
+  const struct pam_message pam_msg = {
+    .msg_style = style,
+    .msg = msg,
+  };
+  const struct pam_conv *pc;
+  struct pam_response *resp;
+
+  if (pam_get_item (pamh, PAM_CONV, (const void **) &pc) != PAM_SUCCESS)
+    return NULL;
+
+  if (!pc || !pc->conv)
+    return NULL;
+
+  if (pc->conv (1, (const struct pam_message *[]){ &pam_msg }, &resp,
+                pc->appdata_ptr) != PAM_SUCCESS)
+    return NULL;
+
+  return resp;
+}
+
 */
 import "C"
 
@@ -30,6 +65,17 @@ import (
 
 // pamHandle allows to pass C.pam_handle_t to this package.
 type pamHandle = *C.pam_handle_t
+
+type PamPrompt int
+
+const (
+	PamPromptEchoOff PamPrompt = 1
+	PamPromptEchoOn  PamPrompt = 2
+	PamPromptError   PamPrompt = 3
+	PamPromptInfo    PamPrompt = 4
+	PamPromptRadio   PamPrompt = 5
+	PamPromptBinary  PamPrompt = 7
+)
 
 func sliceFromArgv(argc C.int, argv **C.char) []string {
 	r := make([]string, 0, argc)
@@ -61,4 +107,49 @@ func getPassword(prompt string) (string, error) {
 	}
 	defer C.free(unsafe.Pointer(cPasswd))
 	return C.GoString(cPasswd), nil
+}
+
+func getModuleName(pamh pamHandle) (string, error) {
+	cModuleName := C.get_module_name(pamh)
+	if cModuleName == nil {
+		return "", fmt.Errorf("no module name found")
+	}
+	return C.GoString(cModuleName), nil
+}
+
+func pamConv(pamh pamHandle, prompt string, kind PamPrompt) (string, error) {
+	cPrompt := C.CString(prompt)
+	defer C.free(unsafe.Pointer(cPrompt))
+	cResponse := C.send_msg(pamh, cPrompt, C.int(kind))
+	if cResponse == nil {
+		return "", fmt.Errorf("conversation with PAM application failed")
+	}
+	defer C.free(unsafe.Pointer(cResponse))
+	return C.GoString(cResponse.resp), nil
+}
+
+func sendInfo(pamh pamHandle, prompt string) error {
+	_, err := pamConv(pamh, "INFO: "+prompt, PamPromptInfo)
+	return err
+}
+
+func sendInfof(pamh pamHandle, format string, args ...interface{}) error {
+	return sendInfo(pamh, fmt.Sprintf(format, args...))
+}
+
+func sendError(pamh pamHandle, prompt string) error {
+	_, err := pamConv(pamh, "ERROR: "+prompt, PamPromptError)
+	return err
+}
+
+func sendErrorf(pamh pamHandle, format string, args ...interface{}) error {
+	return sendInfo(pamh, fmt.Sprintf(format, args...))
+}
+
+func requestInput(pamh pamHandle, prompt string) (string, error) {
+	return pamConv(pamh, prompt+": ", PamPromptEchoOn)
+}
+
+func requestSecret(pamh pamHandle, prompt string) (string, error) {
+	return pamConv(pamh, prompt+": ", PamPromptEchoOff)
 }
