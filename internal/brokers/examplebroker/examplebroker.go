777d66bc3e5b3e1eabb0c8aa05147e364f48aacf
denisonbarbosa <denison.barbosa@canonical.com>
2023-08-04 08:09:42 -0400

    Expose examplebroker types and New() function
    In order to be able to continue using the examplebroker locally, some
    methods need to be public in order to be accessed in the brokers package

--- internal/brokers/examplebroker/examplebroker.go
+++ internal/brokers/examplebroker/examplebroker.go
@@ -30,7 +30,8 @@ type isAuthorizedCtx struct {
 	cancelFunc context.CancelFunc
 }
 
-type broker struct {
+// Broker represents an examplebroker object.
+type Broker struct {
 	currentSessions        map[string]sessionInfo
 	currentSessionsMu      sync.RWMutex
 	userLastSelectedMode   map[string]string
@@ -76,8 +77,9 @@ const (
 	brokerEncryptionKey = "encryptionkey"
 )
 
-func newBroker(name string) (b *broker, fullName, brandIcon string) {
-	return &broker{
+// New creates a new examplebroker object.
+func New(name string) (b *Broker, fullName, brandIcon string) {
+	return &Broker{
 		currentSessions:        make(map[string]sessionInfo),
 		currentSessionsMu:      sync.RWMutex{},
 		userLastSelectedMode:   make(map[string]string),
@@ -88,7 +90,7 @@ func newBroker(name string) (b *broker, fullName, brandIcon string) {
 }
 
 // NewSession creates a new session for the specified user.
-func (b *broker) NewSession(ctx context.Context, username, lang string) (sessionID, encryptionKey string, err error) {
+func (b *Broker) NewSession(ctx context.Context, username, lang string) (sessionID, encryptionKey string, err error) {
 	sessionID = uuid.New().String()
 	b.currentSessionsMu.Lock()
 	b.currentSessions[sessionID] = sessionInfo{
@@ -100,7 +102,7 @@ func (b *broker) NewSession(ctx context.Context, username, lang string) (session
 }
 
 // GetAuthenticationModes returns the list of supported authentication modes for the selected broker depending on session info.
-func (b *broker) GetAuthenticationModes(ctx context.Context, sessionID string, supportedUILayouts []map[string]string) (authenticationModes []map[string]string, err error) {
+func (b *Broker) GetAuthenticationModes(ctx context.Context, sessionID string, supportedUILayouts []map[string]string) (authenticationModes []map[string]string, err error) {
 	sessionInfo, err := b.sessionInfo(sessionID)
 	if err != nil {
 		return nil, err
@@ -261,7 +263,7 @@ func (b *broker) GetAuthenticationModes(ctx context.Context, sessionID string, s
 }
 
 // SelectAuthenticationMode returns the UI layout information for the selected authentication mode.
-func (b *broker) SelectAuthenticationMode(ctx context.Context, sessionID, authenticationModeName string) (uiLayoutInfo map[string]string, err error) {
+func (b *Broker) SelectAuthenticationMode(ctx context.Context, sessionID, authenticationModeName string) (uiLayoutInfo map[string]string, err error) {
 	// Ensure session ID is an active one.
 	sessionInfo, err := b.sessionInfo(sessionID)
 	if err != nil {
@@ -306,7 +308,7 @@ func (b *broker) SelectAuthenticationMode(ctx context.Context, sessionID, authen
 }
 
 // IsAuthorized evaluates the provided authenticationData and returns the authorisation level of the user.
-func (b *broker) IsAuthorized(ctx context.Context, sessionID, authenticationData string) (access, infoUser string, err error) {
+func (b *Broker) IsAuthorized(ctx context.Context, sessionID, authenticationData string) (access, infoUser string, err error) {
 	sessionInfo, err := b.sessionInfo(sessionID)
 	if err != nil {
 		return "", "", err
@@ -347,7 +349,7 @@ func (b *broker) IsAuthorized(ctx context.Context, sessionID, authenticationData
 }
 
 //nolint:unparam // This is an static example implementation, so we don't return an error other than nil.
-func (b *broker) handleIsAuthorized(ctx context.Context, sessionInfo sessionInfo, authData map[string]string) (access, infoUser string, err error) {
+func (b *Broker) handleIsAuthorized(ctx context.Context, sessionInfo sessionInfo, authData map[string]string) (access, infoUser string, err error) {
 	// Note that the "wait" authentication can be cancelled and switch to another mode with a challenge.
 	// Take into account the cancellation.
 	switch sessionInfo.selectedMode {
@@ -448,7 +450,7 @@ func (b *broker) handleIsAuthorized(ctx context.Context, sessionInfo sessionInfo
 }
 
 // EndSession ends the requested session and triggers the necessary clean up steps, if any.
-func (b *broker) EndSession(ctx context.Context, sessionID string) error {
+func (b *Broker) EndSession(ctx context.Context, sessionID string) error {
 	if _, err := b.sessionInfo(sessionID); err != nil {
 		return err
 	}
@@ -466,7 +468,7 @@ func (b *broker) EndSession(ctx context.Context, sessionID string) error {
 
 // CancelIsAuthorized cancels the IsAuthorized request for the specified session.
 // If there is no pending IsAuthorized call for the session, this is a no-op.
-func (b *broker) CancelIsAuthorized(ctx context.Context, sessionID string) {
+func (b *Broker) CancelIsAuthorized(ctx context.Context, sessionID string) {
 	b.isAuthorizedCallsMu.Lock()
 	defer b.isAuthorizedCallsMu.Unlock()
 	if _, exists := b.isAuthorizedCalls[sessionID]; !exists {
@@ -537,7 +539,7 @@ func decryptAES(key []byte, ct string) string {
 }
 
 // sessionInfo returns the session information for the specified session ID or an error if the session is not active.
-func (b *broker) sessionInfo(sessionID string) (sessionInfo, error) {
+func (b *Broker) sessionInfo(sessionID string) (sessionInfo, error) {
 	b.currentSessionsMu.RLock()
 	defer b.currentSessionsMu.RUnlock()
 	session, active := b.currentSessions[sessionID]
