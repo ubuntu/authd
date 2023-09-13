6e7a73b5ae3db666ce49a8af97eaafc529262881
Didier Roche <didier.roche@canonical.com>
2023-09-13 15:21:31 +0200

    Add UpdateFromUserInfo database tests
    Illustrate cases where we can recover and those were we can’t.
    
    Co-authored-by: Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>

--- internal/cache/db_test.go
+++ internal/cache/db_test.go
@@ -102,6 +102,174 @@ func TestNew(t *testing.T) {
 	}
 }
 
+func TestUpdateFromUserInfo(t *testing.T) {
+	t.Parallel()
+
+	userCases := map[string]cache.UserInfo{
+		"user1": {
+			Name:  "user1",
+			UID:   1111,
+			Gecos: "User1 gecos\nOn multiple lines",
+			Dir:   "/home/user1",
+			Shell: "/bin/bash",
+			Groups: []cache.GroupInfo{
+				{Name: "group1", GID: 11111},
+			},
+		},
+		"user1-new-attributes": {
+			Name:  "newuser1",
+			UID:   1111,
+			Gecos: "New user1 gecos",
+			Dir:   "/home/newuser1",
+			Shell: "/bin/dash",
+			Groups: []cache.GroupInfo{
+				{Name: "group1", GID: 11111},
+			},
+		},
+		"group1-new-attributes": {
+			Name:  "user1",
+			UID:   1111,
+			Gecos: "User1 gecos\nOn multiple lines",
+			Dir:   "/home/user1",
+			Shell: "/bin/bash",
+			Groups: []cache.GroupInfo{
+				{Name: "newgroup1", GID: 11111},
+			},
+		},
+		"user1-without-groups": {
+			Name:  "user1",
+			UID:   1111,
+			Gecos: "User1 gecos\nOn multiple lines",
+			Dir:   "/home/user1",
+			Shell: "/bin/bash",
+		},
+		"user1-with-new-group": {
+			Name:  "user1",
+			UID:   1111,
+			Gecos: "User1 gecos\nOn multiple lines",
+			Dir:   "/home/user1",
+			Shell: "/bin/bash",
+			Groups: []cache.GroupInfo{
+				{Name: "group1", GID: 11111},
+				{Name: "group2", GID: 22222},
+			},
+		},
+		"user1-with-new-default-group": {
+			Name:  "user1",
+			UID:   1111,
+			Gecos: "User1 gecos\nOn multiple lines",
+			Dir:   "/home/user1",
+			Shell: "/bin/bash",
+			Groups: []cache.GroupInfo{
+				{Name: "group2", GID: 22222},
+				{Name: "group1", GID: 11111},
+			},
+		},
+		"user1-with-only-new-group": {
+			Name:  "user1",
+			UID:   1111,
+			Gecos: "User1 gecos\nOn multiple lines",
+			Dir:   "/home/user1",
+			Shell: "/bin/bash",
+			Groups: []cache.GroupInfo{
+				{Name: "group2", GID: 22222},
+			},
+		},
+		"user3-without-common-group": {
+			Name:  "user3",
+			UID:   3333,
+			Gecos: "User3 gecos",
+			Dir:   "/home/user3",
+			Shell: "/bin/zsh",
+			Groups: []cache.GroupInfo{
+				{Name: "group3", GID: 33333},
+			},
+		},
+	}
+
+	tests := map[string]struct {
+		userCase string
+		dbFile   string
+
+		wantClearDB bool
+		wantErr     bool
+	}{
+		// New user
+		"Insert new user":                 {userCase: "user1"},
+		"Update last login time for user": {userCase: "user1", dbFile: "one_user_and_group"},
+
+		// User and Group renames
+		"Update user by changing attributes":  {userCase: "user1-new-attributes", dbFile: "one_user_and_group"},
+		"Update group by changing attributes": {userCase: "group1-new-attributes", dbFile: "one_user_and_group"},
+
+		// Group updates
+		"Update user and keep existing groups without specifying them": {userCase: "user1-without-groups", dbFile: "one_user_and_group"},
+		"Update user by adding a new group":                            {userCase: "user1-with-new-group", dbFile: "one_user_and_group"},
+		"Update user by adding a new default group":                    {userCase: "user1-with-new-default-group", dbFile: "one_user_and_group"},
+		"Remove group from user":                                       {userCase: "user1-with-only-new-group", dbFile: "one_user_and_group"},
+
+		// Multi users handling
+		"Update only user even if we have multiple of them":     {userCase: "user1", dbFile: "multiple_users_and_groups"},
+		"Add user to group from another user":                   {userCase: "user1-with-new-group", dbFile: "multiple_users_and_groups"},
+		"Remove user from a group still part from another user": {userCase: "user3-without-common-group", dbFile: "multiple_users_and_groups"},
+
+		// Allowed inconsistent cases
+		"Invalid value entry in groupByID but user restating group recreates entries":       {userCase: "user1", dbFile: "invalid_entry_in_groupByID"},
+		"Invalid value entry in userByID recreates entries":                                 {userCase: "user1", dbFile: "invalid_entry_in_userByID"},
+		"Invalid value entry in groupByName recreates entries":                              {userCase: "user1", dbFile: "invalid_entry_in_groupByName"},
+		"Invalid value entry in groupByName recreates entries even without restating group": {userCase: "user1-without-groups", dbFile: "invalid_entry_in_groupByName"},
+		"Invalid value entry in userByName recreates entries":                               {userCase: "user1", dbFile: "invalid_entry_in_userByName"},
+		"Invalid value entries in other user and groups don't impact current request":       {userCase: "user1", dbFile: "invalid_entries_but_user_and_group1"},
+
+		// Error cases
+		"Error on new user without any groups":                                                     {userCase: "user1-without-groups", wantErr: true},
+		"Error on invalid value entry in userToGroups clear database":                              {userCase: "user1", dbFile: "invalid_entry_in_userToGroups", wantErr: true, wantClearDB: true},
+		"Error on invalid value entry in groupByID with user not restating groups clear database":  {userCase: "user1-without-groups", dbFile: "invalid_entry_in_groupByID", wantErr: true, wantClearDB: true},
+		"Error on invalid value entry in groupToUsers clear database":                              {userCase: "user1", dbFile: "invalid_entry_in_groupToUsers", wantErr: true, wantClearDB: true},
+		"Error on invalid value entry in groupToUsers for user dropping from group clear database": {userCase: "user1", dbFile: "invalid_entry_in_groupToUsers_secondary_group", wantErr: true, wantClearDB: true},
+		"Error on invalid value entry in groupByID for user dropping from group clear database":    {userCase: "user1", dbFile: "invalid_entry_in_groupByID_secondary_group", wantErr: true, wantClearDB: true},
+	}
+	for name, tc := range tests {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			cacheDir := t.TempDir()
+			if tc.dbFile != "" {
+				createDBFile(t, filepath.Join("testdata", tc.dbFile+".db.yaml"), cacheDir)
+			}
+
+			c, err := cache.New(cacheDir)
+			require.NoError(t, err)
+			defer c.Close()
+
+			userInfo := userCases[tc.userCase]
+			err = c.UpdateFromUserInfo(userInfo)
+			if tc.wantErr {
+				require.Error(t, err, "UpdateFromUserInfo should return an error but didn't")
+
+				if tc.wantClearDB {
+					time.Sleep(10 * time.Millisecond)
+					requireNoDirtyFileInDir(t, cacheDir)
+					requireClearedDatabase(t, c)
+				}
+				return
+			} else {
+				require.NoError(t, err)
+			}
+
+			requireNoDirtyFileInDir(t, cacheDir)
+
+			got, err := dumpToYaml(c)
+			require.NoError(t, err, "Created database should be valid yaml content")
+
+			want := testutils.LoadWithUpdateFromGolden(t, got)
+			require.Equal(t, want, got, "Did not get expected database content")
+
+		})
+	}
+}
+
 func createDBFile(t *testing.T, src, destDir string) {
 	t.Helper()
 
@@ -118,6 +286,22 @@ func requireNoDirtyFileInDir(t *testing.T, cacheDir string) {
 
 	_, err := os.Stat(filepath.Join(cacheDir, cache.DirtyFlagDbName))
 	require.ErrorIs(t, err, fs.ErrNotExist, "Dirty flag should have been removed")
+}
+
+func requireClearedDatabase(t *testing.T, c *cache.Cache) {
+	t.Helper()
+
+	want := `GroupByID: {}
+GroupByName: {}
+GroupToUsers: {}
+UserByID: {}
+UserByName: {}
+UserToGroups: {}
+`
+
+	got, err := dumpToYaml(c)
+	require.NoError(t, err, "Created database should be valid yaml content")
+	require.Equal(t, want, string(got), "Database should only have empty buckets")
 }
 
 //go:linkname dumpToYaml github.com/ubuntu/authd/internal/cache.(*Cache).dumpToYaml
