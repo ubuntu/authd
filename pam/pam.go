14d37d3763e02c4dad966c607034c57397821645
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-02-22 14:12:09 +0100

    pam: Do not try to run the module again if we've been already authenticated
    Pam modules can configured to stack other modules and these can be
    called multiple times, so prevent this to happen unless a
    `force_reauth=true` argument is provided.

--- pam/pam.go
+++ pam/pam.go
@@ -35,6 +35,8 @@ const (
 	// PAM module for the second stage authentication to select the default
 	// broker for the current user.
 	authenticationBrokerIDKey = "authentication-broker-id"
+
+	authenticatedKey = "authenticated-key"
 )
 
 func parseArgs(args []string) map[string]string {
@@ -80,7 +82,25 @@ func sendReturnMessageToPam(mTx pam.ModuleTransaction, retStatus adapter.PamRetu
 
 // Authenticate is the method that is invoked during pam_authenticate request.
 func (h *pamModule) Authenticate(mTx pam.ModuleTransaction, flags pam.Flags, args []string) error {
-	return h.handleAuthRequest(authd.SessionMode_AUTH, mTx, flags, args)
+	// Do not try to start authentication again if we've been already through this.
+	// Since PAM modules can be stacked, so we may suffer reentry that is fine but it should
+	// be explicitly allowed.
+	_, err := mTx.GetData(authenticatedKey)
+	if err == nil && parseArgs(args)["force_reauth"] != "true" {
+		return pam.ErrIgnore
+	}
+	if err != nil && !errors.Is(err, pam.ErrNoModuleData) {
+		return err
+	}
+
+	err = h.handleAuthRequest(authd.SessionMode_AUTH, mTx, flags, args)
+	if err != nil && !errors.Is(err, pam.ErrIgnore) {
+		return err
+	}
+	if err := mTx.SetData(authenticatedKey, struct{}{}); err != nil {
+		return err
+	}
+	return err
 }
 
 // ChangeAuthTok is the method that is invoked during pam_sm_chauthtok request.
