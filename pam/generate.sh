fbbfd0da6ceced7f72b19452a17a37287a43bda4
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-03-21 07:53:11 +0100

    debian: Install the pam authd client and pam exec modules and use them
    Switch to use the exec module as default module via the pam exec client.
    
    As per this update the default PAM config and install files.

--- pam/generate.sh
+++ pam/generate.sh
@@ -6,6 +6,7 @@ SCRIPT_PATH=$(dirname "$0")
 PROJECT_ROOT=$(realpath "$SCRIPT_PATH")/..
 module_libname=pam_authd.so
 loader_libname=pam_go_loader.so
+exec_libname=pam_authd_exec.so
 
 cd "$SCRIPT_PATH"
 
@@ -26,8 +27,21 @@ fi
 # shellcheck disable=SC2086
 # we do want to do word splitting on flags
 ${CC:-cc} -o go-loader/"$loader_libname" \
-    go-loader/module.c ${CFLAGS:-} -Wl,--as-needed -Wl,--allow-shlib-undefined \
+    go-loader/module.c ${CFLAGS:--Wall} -Wl,--as-needed -Wl,--allow-shlib-undefined \
     -shared -fPIC -Wl,--unresolved-symbols=report-all \
     -Wl,-soname,"$loader_libname" -lpam ${LDFLAGS:-} "${cc_args[@]}"
 
 chmod 644 go-loader/"$loader_libname"
+
+# shellcheck disable=SC2086
+# we do want to do word splitting on flags
+${CC:-cc} -o go-exec/"$exec_libname" \
+    go-exec/module.c ${CFLAGS:--Wall} \
+    $(pkg-config --cflags gio-2.0 gio-unix-2.0) \
+    -Wl,--as-needed -Wl,--allow-shlib-undefined \
+    -shared -fPIC -Wl,--unresolved-symbols=report-all \
+    -Wl,-soname,"$exec_libname" \
+    $(pkg-config --libs gio-2.0 gio-unix-2.0) \
+    -lpam ${LDFLAGS:-} "${cc_args[@]}"
+
+chmod 644 go-exec/"$exec_libname"
