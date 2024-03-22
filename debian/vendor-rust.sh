ff24b5b368cfd0b5ee9b578a7811aa15a54fe41b
Didier Roche <didier.roche@canonical.com>
2024-03-22 11:40:32 +0100

    Remove rechecksumming logic as now handled when vendoring
    The third party tool does the recompute the checksum for us while
    vendoring. We do not need to redo it.

--- debian/vendor-rust.sh
+++ debian/vendor-rust.sh
@@ -16,16 +16,3 @@ fi
 # This results in cargo failing to compile, since the files (which are listed in the checksums) are not there anymore.
 # For those crates, we need to replace their checksum with a more general one that only lists the crate checksum, instead of each file.
 ${CARGO_PATH} vendor-filterer "${CARGO_VENDOR_DIR}"
-
-[ ! -e "${DH_CARGO_VENDORED_SOURCES}" ] || ${DH_CARGO_VENDORED_SOURCES}
-[ -e /usr/bin/jq ] || (echo "jq is required to run this script. Try installing it with 'sudo apt install jq'" && exit 1)
-
-for dep in vendor_rust/*; do
-    checksum_file="${dep}/.cargo-checksum.json"
-    a_files=$(jq '.files | keys | map(select(.|test(".a$")))' "${checksum_file}")
-    if [ "$a_files" = "[]" ]; then
-        continue
-    fi
-    pkg_checksum=$(jq '.package' "${checksum_file}")
-    echo "{\"files\": {}, \"package\": ${pkg_checksum}}" > "${checksum_file}"
-done
