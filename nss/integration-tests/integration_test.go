9fa350e3d3952db636bd4f697c1e4e1e4108b715
Didier Roche <didier.roche@canonical.com>
2024-04-15 08:52:52 +0200

    NSS as non root integration tests

--- nss/integration-tests/integration_test.go
+++ nss/integration-tests/integration_test.go
@@ -43,26 +43,29 @@ func TestIntegration(t *testing.T) {
 		key     string
 		cacheDB string
 
-		noDaemon       bool
-		wantSecondCall bool
-		shouldPreCheck bool
+		noDaemon           bool
+		currentUserNotRoot bool
+		wantSecondCall     bool
+		shouldPreCheck     bool
 
 		wantErr bool
 	}{
-		"Get all entries from passwd": {db: "passwd"},
-		"Get all entries from group":  {db: "group"},
-		"Get all entries from shadow": {db: "shadow"},
+		"Get all entries from passwd":                    {db: "passwd"},
+		"Get all entries from group":                     {db: "group"},
+		"Get all entries from shadow if considered root": {db: "shadow"},
 
-		"Get entry from passwd by name": {db: "passwd", key: "user1"},
-		"Get entry from group by name":  {db: "group", key: "group1"},
-		"Get entry from shadow by name": {db: "shadow", key: "user1"},
+		"Get entry from passwd by name":                    {db: "passwd", key: "user1"},
+		"Get entry from group by name":                     {db: "group", key: "group1"},
+		"Get entry from shadow by name if considered root": {db: "shadow", key: "user1"},
 
 		"Get entry from passwd by id": {db: "passwd", key: "1111"},
 		"Get entry from group by id":  {db: "group", key: "11111"},
 
 		"Check user with broker if not found in cache": {db: "passwd", key: "user-pre-check", shouldPreCheck: true},
 
-		// Even though those are "error" cases, the getent command won't fail since the other databases on the machine will return some entries.
+		// Even though those are "error" cases, the getent command won't fail when trying to list content of a service.
+		"Returns empty when getting all entries from shadow if regular user": {db: "shadow", currentUserNotRoot: true},
+
 		"Returns empty when getting all entries from passwd and daemon is not available": {db: "passwd", noDaemon: true},
 		"Returns empty when getting all entries from group and daemon is not available":  {db: "group", noDaemon: true},
 		"Returns empty when getting all entries from shadow and daemon is not available": {db: "shadow", noDaemon: true},
@@ -73,6 +76,8 @@ func TestIntegration(t *testing.T) {
 
 		/* Error cases */
 		// We can't assert on the returned error type since the error returned by getent will always be 2 (i.e. Not Found), even though the library returns other types.
+		"Error when getting shadow by name if regular user": {db: "shadow", key: "user1", currentUserNotRoot: true, wantErr: true},
+
 		"Error when getting passwd by name and entry does not exist":                        {db: "passwd", key: "doesnotexit", wantErr: true},
 		"Error when getting passwd by name entry exists in broker but precheck is disabled": {db: "passwd", key: "user-pre-check", wantErr: true},
 		"Error when getting group by name and entry does not exist":                         {db: "group", key: "doesnotexit", wantErr: true},
@@ -95,7 +100,7 @@ func TestIntegration(t *testing.T) {
 			socketPath := defaultSocket
 
 			var useAlternativeDaemon bool
-			if tc.cacheDB != "" {
+			if tc.cacheDB != "" || tc.currentUserNotRoot {
 				useAlternativeDaemon = true
 			} else {
 				tc.cacheDB = defaultDbState
@@ -114,7 +119,10 @@ func TestIntegration(t *testing.T) {
 
 				var daemonStopped chan struct{}
 				ctx, cancel := context.WithCancel(context.Background())
-				env := append(grouptests.GPasswdMockEnv(t, outPath, groupsFilePath), "AUTHD_INTEGRATIONTESTS_CURRENT_USER_AS_ROOT=1")
+				env := grouptests.GPasswdMockEnv(t, outPath, groupsFilePath)
+				if !tc.currentUserNotRoot {
+					env = append(env, "AUTHD_INTEGRATIONTESTS_CURRENT_USER_AS_ROOT=1")
+				}
 				socketPath, daemonStopped = testutils.RunDaemon(ctx, t, daemonPath,
 					testutils.WithPreviousDBState(tc.cacheDB),
 					testutils.WithEnvironment(env...),
