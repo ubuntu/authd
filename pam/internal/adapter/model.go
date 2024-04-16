98bb2c3cbffa781108e5916edb383a2274e5cb3e
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-04-16 16:00:44 +0200

    model: Start broker selection only if required
    Delay broker selection until something requires it, so that we don't
    need to write anything to the terminal if there's no need to select the
    broker

--- pam/internal/adapter/model.go
+++ pam/internal/adapter/model.go
@@ -171,9 +171,7 @@ func (m *UIModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 
 		// Got user and brokers? Time to auto or manually select.
-		return m, tea.Sequence(
-			m.changeStage(pam_proto.Stage_brokerSelection),
-			AutoSelectForUser(m.Client, m.username()))
+		return m, AutoSelectForUser(m.Client, m.username())
 
 	case BrokerSelected:
 		log.Debugf(context.TODO(), "%#v", msg)
