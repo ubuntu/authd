1c8123875cbce0fbc06a36542f8e8ec80dfa6977
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-06-19 23:31:14 +0200

    pam/integration-tests: Run passwd_unexisting_user tape with CLI
    We had this tape in the repository, but it wasn't used, so actually use
    it

--- pam/integration-tests/cli_test.go
+++ pam/integration-tests/cli_test.go
@@ -161,6 +161,7 @@ func TestCLIChangeAuthTok(t *testing.T) {
 		"Retry if new password does not match quality criteria": {tape: "passwd_bad_password"},
 
 		"Prevent change password if auth fails":                                     {tape: "passwd_auth_fail"},
+		"Prevent change password if user does not exist":                            {tape: "passwd_unexistent_user"},
 		"Prevent change password if current user is not root as can't authenticate": {tape: "passwd_not_root", currentUserNotRoot: true},
 
 		"Exit authd if local broker is selected": {tape: "passwd_local_broker"},
