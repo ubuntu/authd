49466ac319c4637593b086bccf56cd7ad73b0e51
Gabriel Nagy <gabriel.nagy@canonical.com>
2024-06-25 23:28:20 +0300

    Rework newpasswd view for CLI
    Instead of showing both prompts from the get-go, and having Enter submit
    the form straight away (and only being able to navigate through it with
    Tab), such as:
    
        Enter your new password
        > ****
        >
    
    We now render a single input if the user has just been shown the form,
    with an updated prompt:
    
        Enter your new password
    
        New password:
        > ****
    
    When pressing Enter -- provided that the password passes our quality
    checks -- the form changes to:
    
        Enter your new password (confirm)
    
        New password:
        > ****
        Confirm password:
        > ****
    
    This makes it clearer that we want the user to re-type their password.
    As a result, Tab navigation is no longer supported in non-skippable
    views, and the form can only be advanced by pressing Enter.
    
    For skippable views (i.e. with a button), Tab only works if the password
    is not filled in. This avoids opening a can of worms regarding to what
    fields we should be showing or not based on whether password fields are
    filled in. Thus, the skippable experience becomes:
    
        Enter your new password (3 days until mandatory)
    
        New password:
        >
    
        [ Skip ]  // Focusable
    
    and
    
        Enter your new password (3 days until mandatory)
    
        New password:
        > *****
    
        [ Skip ]  // No longer focusable, have to finish filling in the form
                  // (or clear it)
    
    Otherwise, I believe we should keep the form as simple as possible,
    without the possibility of jumping back to the previous password field
    once it was filled in, at the sake of improved maintainability.
    
    Fixes #365 / UDENG-3112

--- pam/internal/adapter/newpasswordmodel.go
+++ pam/internal/adapter/newpasswordmodel.go
@@ -9,10 +9,12 @@ import (
 
 // newPasswordModel is the form layout type to allow authentication and return a challenge.
 type newPasswordModel struct {
-	errorMsg string
-	label    string
+	errorMsg  string
+	label     string
+	skippable bool
 
 	passwordEntries []*textinputModel
+	passwordLabels  []string
 	focusableModels []authenticationComponent
 	focusIndex      int
 }
@@ -21,31 +23,30 @@ type newPasswordModel struct {
 func newNewPasswordModel(label, entryType, buttonLabel string) newPasswordModel {
 	var focusableModels []authenticationComponent
 	var passwordEntries []*textinputModel
+	var skippable bool
 
 	// TODO: add digits and force validation.
 	for range []int{0, 1} {
-		switch entryType {
-		case "chars":
-			entry := &textinputModel{Model: textinput.New()}
-			passwordEntries = append(passwordEntries, entry)
-			focusableModels = append(focusableModels, entry)
-		case "chars_password":
-			entry := &textinputModel{Model: textinput.New()}
-			passwordEntries = append(passwordEntries, entry)
+		entry := &textinputModel{Model: textinput.New()}
+		if entryType == "chars_password" {
 			entry.EchoMode = textinput.EchoPassword
-			focusableModels = append(focusableModels, entry)
 		}
+		passwordEntries = append(passwordEntries, entry)
+		focusableModels = append(focusableModels, entry)
 	}
 
 	if buttonLabel != "" {
+		skippable = true
 		button := &buttonModel{label: buttonLabel}
 		focusableModels = append(focusableModels, button)
 	}
 
 	return newPasswordModel{
-		label: label,
+		label:     label,
+		skippable: skippable,
 
 		passwordEntries: passwordEntries,
+		passwordLabels:  []string{"New password:", "Confirm password:"},
 		focusableModels: focusableModels,
 	}
 }
@@ -72,26 +73,53 @@ func (m newPasswordModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	case tea.KeyMsg: // Key presses
 		switch msg.String() {
-		case "enter":
-			if m.focusIndex >= len(m.focusableModels) {
+		case "tab", "shift+tab":
+			// Only allow tabbing if the form is skippable
+			if !m.skippable {
 				return m, nil
 			}
+
+			m.errorMsg = ""
+
+			// Only allow tabbing if no password was entered
+			for _, pe := range m.passwordEntries {
+				if pe.Value() != "" {
+					return m, nil
+				}
+			}
+
+			if m.focusIndex == 0 {
+				return m, m.focusPrevField()
+			}
+			return m, m.focusNextField()
+
+		case "enter":
 			entry := m.focusableModels[m.focusIndex]
 			switch entry := entry.(type) {
 			case *textinputModel:
-				// Check if the password is empty
-				if m.passwordEntries[0].Value() == "" {
-					m.Clear()
-					return m, sendEvent(errMsgToDisplay{msg: "Password must not be empty"})
-				}
-
-				// Check both entries are matching
-				if m.passwordEntries[0].Value() != m.passwordEntries[1].Value() {
-					m.Clear()
-					return m, sendEvent(errMsgToDisplay{msg: "Password entries don't match"})
-				}
-
 				m.errorMsg = ""
+
+				// First entry is focused
+				if m.focusIndex == 0 {
+					// Check if the password is empty
+					if m.passwordEntries[0].Value() == "" {
+						m.Clear()
+						return m, sendEvent(errMsgToDisplay{msg: "Password must not be empty"})
+					}
+
+					// Check password quality
+					return m, sendEvent(newPasswordCheck{m.passwordEntries[0].Value()})
+				}
+
+				// Second entry is focused
+				if m.focusIndex == 1 {
+					// Check both entries are matching
+					if m.passwordEntries[0].Value() != m.passwordEntries[1].Value() {
+						m.Clear()
+						return m, sendEvent(errMsgToDisplay{msg: "Password entries don't match"})
+					}
+				}
+
 				return m, sendEvent(isAuthenticatedRequested{
 					item: &authd.IARequest_AuthenticationData_Challenge{Challenge: entry.Value()},
 				})
@@ -103,14 +131,6 @@ func (m newPasswordModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 
 			return m, nil
-
-		case "tab":
-			m.errorMsg = ""
-			if m.focusIndex == 0 && m.passwordEntries[0].Value() != "" {
-				return m, sendEvent(newPasswordCheck{m.passwordEntries[0].Value()})
-			}
-
-			return m, m.focusNextField()
 
 		default:
 			m.errorMsg = ""
@@ -132,14 +152,22 @@ func (m newPasswordModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 // View renders a text view of the form.
 func (m newPasswordModel) View() string {
-	fields := []string{m.label}
-	for _, fm := range m.focusableModels {
-		fields = append(fields, fm.View())
+	fields := []string{m.label, ""}
+
+	for i, fm := range m.focusableModels {
+		switch entry := fm.(type) {
+		case *textinputModel:
+			// Only show the second password entry if the first one is filled
+			// (in case the form is advanced using Tab)
+			if m.focusIndex == 1 && m.passwordEntries[0].Value() != "" || i == 0 {
+				fields = append(fields, []string{m.passwordLabels[i], entry.View()}...)
+			}
+		case *buttonModel:
+			fields = append(fields, entry.View())
+		}
 	}
 
-	return lipgloss.JoinVertical(lipgloss.Left,
-		fields...,
-	)
+	return lipgloss.JoinVertical(lipgloss.Left, fields...)
 }
 
 // Focus focuses this model.
@@ -158,12 +186,16 @@ func (m newPasswordModel) Blur() {
 	m.focusableModels[m.focusIndex].Blur()
 }
 
-func (m *newPasswordModel) focusNextField() tea.Cmd {
+func (m *newPasswordModel) focusField(increment int) tea.Cmd {
 	var cmd tea.Cmd
-	m.focusIndex++
-	if m.focusIndex == len(m.focusableModels) {
-		m.focusIndex = 0
+	focusableLen := len(m.focusableModels)
+
+	// Wrap around
+	m.focusIndex += increment
+	if m.focusIndex < 0 || m.focusIndex >= focusableLen {
+		m.focusIndex = (m.focusIndex%focusableLen + focusableLen) % focusableLen
 	}
+
 	for i, fm := range m.focusableModels {
 		if i != m.focusIndex || cmd != nil {
 			fm.Blur()
@@ -172,6 +204,14 @@ func (m *newPasswordModel) focusNextField() tea.Cmd {
 		cmd = fm.Focus()
 	}
 	return cmd
+}
+
+func (m *newPasswordModel) focusNextField() tea.Cmd {
+	return m.focusField(1)
+}
+
+func (m *newPasswordModel) focusPrevField() tea.Cmd {
+	return m.focusField(-1)
 }
 
 func (m *newPasswordModel) Clear() {
