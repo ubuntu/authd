f3ceeab65ae054c1b68152eaf233be0e6dc7db08
denisonbarbosa <denison.barbosa@canonical.com>
2024-06-26 08:02:56 -0400

    Fix supported layout for the QR Code
    The additional code entry in the layout must be optional, not required.
    Otherwise this invalidates QR Codes that do not have any code associated
    with them.

--- pam/internal/adapter/authmodeselection.go
+++ pam/internal/adapter/authmodeselection.go
@@ -102,7 +102,7 @@ func (m *authModeSelectionModel) Init() tea.Cmd {
 				{
 					Type:    "qrcode",
 					Content: &required,
-					Code:    &required,
+					Code:    &optional,
 					Wait:    &requiredWithBooleans,
 					Label:   &optional,
 					Button:  &optional,
