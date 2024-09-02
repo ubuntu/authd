e91ab76c81b8a54e2a4ed16487de349ff3f2c0bd
Adrian Dombeck <adrian.dombeck@canonical.com>
2024-09-02 12:23:42 +0200

    Don't check if username matches the requested username
    It's the broker's responsibility to do that and authd doesn't know which
    usernames the provider considers equal, for example if they are
    case-sensitive or not.

--- pam/integration-tests/cli_test.go
+++ pam/integration-tests/cli_test.go
@@ -65,7 +65,6 @@ func TestCLIAuthenticate(t *testing.T) {
 
 		"Deny authentication if max attempts reached":                         {tape: "max_attempts"},
 		"Deny authentication if user does not exist":                          {tape: "unexistent_user"},
-		"Deny authentication if usernames dont match":                         {tape: "mismatch_username"},
 		"Deny authentication if newpassword does not match required criteria": {tape: "bad_password"},
 
 		"Exit authd if local broker is selected": {tape: "local_broker"},
