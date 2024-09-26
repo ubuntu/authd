6bd0f3d8439f0a287594fb0ed2939d960076916a
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-09-26 23:27:41 +0200

    pam/integration-tests: Add PAM tests using SSHd
    We have tests simulating SSH behavior, but it's definitely better to
    ensure that SSH works as expected using the actual server and client
    when used with authd.
    
    In order to get sshd to be fully usable for this simulation, however, we
    need to "mock" it by using a LD_PRELOAD'ed library that has to be in C
    (as the cgo version I initially done would trigger the well known issues
    we have with go libraries and threads) and that we use it for mocking
    the sshd requests on getpwnam and to make sshd to open our pam file
    (that is hardcoded in sshd).
    
    To handle the getpwnam we could even have used __nss_configure_lookup()
    with a fake module or our own, but this is just a simpler solution for
    now, while in future we may want to add full integration tests where
    also our own NSS library is used instead, but this was outside the scope
    of this change, that is mainly focused on the behavior of the PAM module
    only.
    
    As for the rest, just repeat all the native tests that make sense using
    SSH instead, by de facto re-using the same tape files, minus the removal
    of the user selection.

---
+++ pam/integration-tests/sshd_preloader/sshd_preloader.c
@@ -0,0 +1,105 @@
+#define _GNU_SOURCE 1
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dlfcn.h>
+#include <string.h>
+#include <pwd.h>
+#include <limits.h>
+
+#define AUTHD_TEST_SHELL "/bin/sh"
+#define AUTHD_TEST_GECOS ""
+#define AUTHD_DEFAULT_SSH_PAM_SERVICE_NAME "sshd"
+
+__attribute__((constructor))
+void constructor (void)
+{
+  fprintf (stderr, "sshd_preloader: Library loaded (parent pid :%d)!\n",
+           getpid ());
+}
+
+__attribute__((destructor))
+void destructor (void)
+{
+  fprintf (stderr, "sshd_preloader: Library unloaded  (parent pid :%d)!\n",
+           getpid ());
+}
+
+/*
+ * Note: none of this code is meant to be thread-safe, but we don't need it for
+ * the way we're using it in our tests.
+ */
+
+static const char *
+get_home_path (void)
+{
+  const char *home_path = getenv ("AUTHD_TEST_SSH_HOME");
+
+  if (home_path == NULL)
+    return "/not-existing-home";
+
+  return home_path;
+}
+
+struct passwd *
+getpwnam (const char *name)
+{
+  static const char *test_user = NULL;
+
+  if (!test_user)
+    test_user = getenv ("AUTHD_TEST_SSH_USER");
+
+  if (test_user == NULL || strcmp (test_user, name) != 0)
+    {
+      struct passwd * (*orig_getpwnam) (const char *name);
+
+      orig_getpwnam = dlsym (RTLD_NEXT, "getpwnam");
+      return orig_getpwnam (name);
+    }
+
+  fprintf (stderr, "sshd_preloader: Simulating to be user %s\n", name);
+
+  static struct passwd passwd_entity = {
+    .pw_shell = AUTHD_TEST_SHELL,
+    .pw_gecos = AUTHD_TEST_GECOS,
+  };
+
+  /* We're simulating to be the same user running the test but with another
+   * name and HOME directory, so that we won't touch the user settings, but
+   * it's still enough to trick sshd.
+   */
+  passwd_entity.pw_uid = getuid ();
+  passwd_entity.pw_gid = getgid ();
+  passwd_entity.pw_name = (char *) name;
+  passwd_entity.pw_dir = (char *) get_home_path ();
+
+  return &passwd_entity;
+}
+
+FILE *
+fopen (const char *pathname, const char *mode)
+{
+  static FILE * (*orig_fopen) (const char *pathname, const char *mode) = NULL;
+  const char *service_path;
+
+  if (!orig_fopen)
+    orig_fopen = dlsym (RTLD_NEXT, "fopen");
+
+  service_path = getenv ("AUTHD_TEST_SSH_PAM_SERVICE");
+
+  if (service_path == NULL || pathname == NULL)
+    return orig_fopen (pathname, mode);
+
+  if (strcmp (pathname, "/etc/pam.d/" AUTHD_DEFAULT_SSH_PAM_SERVICE_NAME) == 0 ||
+      strcmp (pathname, "/usr/lib/pam.d/" AUTHD_DEFAULT_SSH_PAM_SERVICE_NAME) == 0)
+    {
+      fprintf (stderr, "sshd_preloader: Trying to open '%s', "
+               "but redirecting instead to '%s'\n", pathname, service_path);
+      pathname = service_path;
+    }
+
+  return orig_fopen (pathname, mode);
+}
+
