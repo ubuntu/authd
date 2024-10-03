ubuntu/authd

commit ca47562d315307f036812296b164633811269cc1

Don't log "IsAuthenticatedRequested" poll result
It contains the user password

pam/internal/adapter/gdmmodel.go
@@ -96,8 +96,14 @@ func (m *gdmModel) pollGdm() tea.Cmd {
 		})
 	}
 
-	if len(gdmPollResults) > 0 {
-		log.Debugf(context.TODO(), "Gdm Poll response is %v", gdmPollResults)
+	for _, result := range gdmPollResults {
+		// Don't log EventData_IsAuthenticatedRequested because it contains
+		// the user password
+		if result.GetIsAuthenticatedRequested() != nil {
+			log.Debugf(context.TODO(), "GDM poll returned: IsAuthenticatedRequested")
+		} else {
+			log.Debugf(context.TODO(), "GDM poll returned: %s", result.Data)
+		}
 	}
 
 	commands := []tea.Cmd{sendEvent(gdmPollDone{})}

commit cbaf0d4c2a972d2e71d23625f5fc91d93880109b

pam/gdmmodel: Iterate over poll results only if debugging is enabled
Fixes: ca47562d3153

pam/internal/adapter/gdmmodel.go
@@ -96,13 +96,15 @@ func (m *gdmModel) pollGdm() tea.Cmd {
 		})
 	}
 
-	for _, result := range gdmPollResults {
-		// Don't log EventData_IsAuthenticatedRequested because it contains
-		// the user password
-		if result.GetIsAuthenticatedRequested() != nil {
-			log.Debugf(context.TODO(), "GDM poll returned: IsAuthenticatedRequested")
-		} else {
-			log.Debugf(context.TODO(), "GDM poll returned: %s", result.Data)
+	if log.IsLevelEnabled(log.DebugLevel) {
+		for _, result := range gdmPollResults {
+			// Don't log EventData_IsAuthenticatedRequested because it contains
+			// the user password
+			if result.GetIsAuthenticatedRequested() != nil {
+				log.Debugf(context.TODO(), "GDM poll returned: IsAuthenticatedRequested")
+			} else {
+				log.Debugf(context.TODO(), "GDM poll returned: %s", result.Data)
+			}
 		}
 	}
 