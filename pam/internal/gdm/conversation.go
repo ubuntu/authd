ubuntu/authd

commit 40fa85db968ffb87ef5d4f3b7092671a6faa01ed

pam: Don't log polls
When debug mode is enabled for the PAM module, these polls spam the
journal a lot.

This also fixes that the user password is logged as part of the
"isAuthenticatedRequested" poll response.

pam/internal/gdm/conversation.go
@@ -47,7 +47,11 @@ func sendData(pamMTx pam.ModuleTransaction, d *Data) ([]byte, error) {
 		return nil, err
 	}
 
-	log.Debugf(context.TODO(), "Sending to GDM: %s", bytes)
+	// Log unless it's a poll, which are so frequently that it would be
+	// too verbose to log them.
+	if d.Type != DataType_poll {
+		log.Debugf(context.TODO(), "Sending to GDM: %s", bytes)
+	}
 	return sendToGdm(pamMTx, bytes)
 }
 
@@ -59,7 +63,11 @@ func SendData(pamMTx pam.ModuleTransaction, d *Data) (*Data, error) {
 	}
 
 	gdmData, err := NewDataFromJSON(jsonValue)
-	log.Debugf(context.TODO(), "Got from GDM: %s", jsonValue)
+	// Log unless it's a poll, which are so frequently that it would be
+	// too verbose to log them.
+	if d.Type != DataType_poll {
+		log.Debugf(context.TODO(), "Got from GDM: %s", jsonValue)
+	}
 	if err != nil {
 		return nil, err
 	}

pam/internal/adapter/gdmmodel.go
@@ -95,7 +95,10 @@ func (m *gdmModel) pollGdm() tea.Cmd {
 			msg:    fmt.Sprintf("Sending GDM poll failed: %v", err),
 		})
 	}
-	log.Debugf(context.TODO(), "Gdm Poll response is %v", gdmPollResults)
+
+	if len(gdmPollResults) > 0 {
+		log.Debugf(context.TODO(), "Gdm Poll response is %v", gdmPollResults)
+	}
 
 	commands := []tea.Cmd{sendEvent(gdmPollDone{})}
 

commit c608853d2b6db17120e3a930f924f41dca331f02

pam/gdm/conversation: Show the Poll response if non-empty
In case the gdm data poll response has content, it's still something we
want to show, even though we should sanitize the value when it contains
the challenge secret

Fixes: 40fa85db968ff

pam/internal/gdm/conversation.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"regexp"
 	"sync/atomic"
 
 	"github.com/msteinert/pam/v2"
@@ -13,6 +14,7 @@ import (
 )
 
 var conversations atomic.Int32
+var challengeRegex = regexp.MustCompile(`"challenge"\s*:\s*"(?:[^"\\]|\\.)*"`)
 
 // ConversationInProgress checks if conversations are currently active.
 func ConversationInProgress() bool {
@@ -63,9 +65,16 @@ func SendData(pamMTx pam.ModuleTransaction, d *Data) (*Data, error) {
 	}
 
 	gdmData, err := NewDataFromJSON(jsonValue)
-	// Log unless it's a poll, which are so frequently that it would be
+	// Log unless it's an empty poll, which are so frequently that it would be
 	// too verbose to log them.
-	if d.Type != DataType_poll {
+	if gdmData.Type == DataType_pollResponse && len(gdmData.GetPollResponse()) == 0 {
+		jsonValue = nil
+	}
+	if log.IsLevelEnabled(log.DebugLevel) && jsonValue != nil &&
+		gdmData != nil && gdmData.Type == DataType_pollResponse {
+		jsonValue = challengeRegex.ReplaceAll(jsonValue, []byte(`"challenge":"**************"`))
+	}
+	if jsonValue != nil {
 		log.Debugf(context.TODO(), "Got from GDM: %s", jsonValue)
 	}
 	if err != nil {