ac84d80a143d8fb46afe200c344a37bbef63a89f
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-10-09 00:28:25 +0200

    examplebroker: Add user-needs-reset2
    So we can use it for multiple needs-reset tests

--- examplebroker/broker.go
+++ examplebroker/broker.go
@@ -101,6 +101,7 @@ var (
 		"user-mfa":              {Password: "goodpass"},
 		"user-mfa-with-reset":   {Password: "goodpass"},
 		"user-needs-reset":      {Password: "goodpass"},
+		"user-needs-reset2":     {Password: "goodpass"},
 		"user-can-reset":        {Password: "goodpass"},
 		"user-can-reset2":       {Password: "goodpass"},
 		"user-local-groups":     {Password: "goodpass"},
@@ -161,6 +162,8 @@ func (b *Broker) NewSession(ctx context.Context, username, lang, mode string) (s
 	case "user-mfa":
 		info.neededAuthSteps = 3
 	case "user-needs-reset":
+		fallthrough
+	case "user-needs-reset2":
 		info.neededAuthSteps = 2
 		info.pwdChange = mustReset
 	case "user-can-reset":
