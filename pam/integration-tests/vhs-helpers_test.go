7cd8b138233a7a8ba5b10c9cec8d6d74fbac1c74
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-10-15 17:05:36 +0200

    pam/integration-tests: Fix replacement of sleep variables
    Only replace the variable and the math operation itself instead of the
    whole line

--- pam/integration-tests/vhs-helpers_test.go
+++ pam/integration-tests/vhs-helpers_test.go
@@ -91,7 +91,7 @@ var (
 	}
 
 	vhsSleepRegex = regexp.MustCompile(
-		`(?m)\$\{?(AUTHD_SLEEP_[A-Z_]+)\}?(\s?([*/]+)\s?([\d.]+))?.*$`)
+		`(?m)\$\{?(AUTHD_SLEEP_[A-Z_]+)\}?(\s?([*/]+)\s?([\d.]+))?(.*)$`)
 	vhsEmptyLinesRegex = regexp.MustCompile(`(?m)((^\n^\n)+(^\n)?|^\n)(^─+$)`)
 )
 
@@ -347,7 +347,7 @@ func evaluateTapeVariables(t *testing.T, tapeString string, td tapeData) string 
 	}
 
 	for _, m := range vhsSleepRegex.FindAllStringSubmatch(tapeString, -1) {
-		fullMatch, sleepKind, op, arg := m[0], m[1], m[3], m[4]
+		fullMatch, sleepKind, op, arg, rest := m[0], m[1], m[3], m[4], m[5]
 		sleep, ok := (*sleepValues)[sleepKind]
 		require.True(t, ok, "Setup: unknown sleep kind: %q", sleepKind)
 
@@ -369,7 +369,7 @@ func evaluateTapeVariables(t *testing.T, tapeString string, td tapeData) string 
 
 		replaceRegex := regexp.MustCompile(fmt.Sprintf(`(?m)%s$`, regexp.QuoteMeta(fullMatch)))
 		tapeString = replaceRegex.ReplaceAllString(tapeString,
-			fmt.Sprintf("%dms", sleepDuration(sleep).Milliseconds()))
+			fmt.Sprintf("%dms%s", sleepDuration(sleep).Milliseconds(), rest))
 	}
 
 	if td.Command == "" {
