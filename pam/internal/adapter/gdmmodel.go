2cd9c710e0a26543a5bef28902ea94195ed48625
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-11-13 17:01:45 +0100

    gdmmodel: Use utility function we already have for user selection

--- pam/internal/adapter/gdmmodel.go
+++ pam/internal/adapter/gdmmodel.go
@@ -107,9 +107,7 @@ func (m *gdmModel) pollGdm() tea.Cmd {
 	for _, result := range gdmPollResults {
 		switch res := result.Data.(type) {
 		case *gdm.EventData_UserSelected:
-			commands = append(commands, sendEvent(userSelected{
-				username: res.UserSelected.UserId,
-			}))
+			commands = append(commands, sendUserSelected(res.UserSelected.UserId))
 
 		case *gdm.EventData_BrokerSelected:
 			if res.BrokerSelected == nil {
