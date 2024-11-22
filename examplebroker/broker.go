c0ba8dd2b16e11e45a9ec6b3c77b2bbb7f8cfa4c
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-11-22 12:09:33 +0100

    examplebroker: Move users to another file
    We can then expose these cleanly

--- examplebroker/broker.go
+++ examplebroker/broker.go
@@ -95,28 +95,6 @@ type Broker struct {
 	sleepMultiplier float64
 }
 
-type userInfoBroker struct {
-	Password string
-}
-
-var (
-	exampleUsersMu = sync.RWMutex{}
-	exampleUsers   = map[string]userInfoBroker{
-		"user1":               {Password: "goodpass"},
-		"user2":               {Password: "goodpass"},
-		"user3":               {Password: "goodpass"},
-		"user-mfa":            {Password: "goodpass"},
-		"user-mfa-with-reset": {Password: "goodpass"},
-		"user-needs-reset":    {Password: "goodpass"},
-		"user-needs-reset2":   {Password: "goodpass"},
-		"user-can-reset":      {Password: "goodpass"},
-		"user-can-reset2":     {Password: "goodpass"},
-		"user-local-groups":   {Password: "goodpass"},
-		"user-pre-check":      {Password: "goodpass"},
-		"user-sudo":           {Password: "goodpass"},
-	}
-)
-
 var (
 	passwordMode = authMode{
 		id:             "password",
