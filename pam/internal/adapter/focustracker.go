0fadf0f773ee493464ef83f6f073c781e10fc522
Marco Trevisan (Treviño) <mail@3v1n0.net>
2025-02-27 11:39:29 +0100

    pam/authentication: Add a dummy focusTracker module when using GDM or PAM
    Wheen using the gdm or native modules we still use the authentication
    model as our data model to track the focused widget status, however
    sine we don't have any current model set in such case, we end up never
    marking the "Authentication" model as ever tracked.
    
    This has implications in the model stage change, since we do use the
    focus state to figure out what is the current stage.
    
    In these case add then a very simple focus tracker stage so that the
    authentication state can still marked as focused when it's the case.
    
    This should fix some "focus jumps" we may see in some cases

---
+++ pam/internal/adapter/focustracker.go
@@ -0,0 +1,45 @@
+package adapter
+
+import (
+	"context"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/ubuntu/authd/log"
+)
+
+type focusTrackerModel struct {
+	focused bool
+}
+
+// Init initializes the model.
+func (m *focusTrackerModel) Init() tea.Cmd {
+	return nil
+}
+
+// Update handles events and actions.
+func (m *focusTrackerModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return convertTo[tea.Model](m), nil
+}
+
+// View renders the model.
+func (m *focusTrackerModel) View() string {
+	return ""
+}
+
+// Focus focuses the model.
+func (m *focusTrackerModel) Focus() tea.Cmd {
+	log.Debugf(context.TODO(), "%T: Focus", m)
+	m.focused = true
+	return nil
+}
+
+// Focused returns whether the model is focused.
+func (m *focusTrackerModel) Focused() bool {
+	return m.focused
+}
+
+// Blur blurs the model.
+func (m *focusTrackerModel) Blur() {
+	log.Debugf(context.TODO(), "%T: Blur", m)
+	m.focused = false
+}
