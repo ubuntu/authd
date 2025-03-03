b26a0b6f2ccb15220654cae07df690d3b19716af
Marco Trevisan (Treviño) <mail@3v1n0.net>
2025-03-03 20:23:13 +0100

    pam/integration-tests/exec: Add tests checking child behavior on signals
    When the child receives signals should ignore them and just return a
    generic PAM system error, but still we need to check that they're
    properly handled

--- pam/integration-tests/cmd/exec-client/modulewrapper.go
+++ pam/integration-tests/cmd/exec-client/modulewrapper.go
@@ -5,8 +5,12 @@ package main
 import (
 	"context"
 	"errors"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/msteinert/pam/v2"
+	"github.com/ubuntu/authd/log"
 	"github.com/ubuntu/authd/pam/internal/dbusmodule"
 )
 
@@ -27,4 +31,31 @@ func (m *moduleWrapper) SimulateClientPanic(text string) {
 // SimulateClientError forces the client to return a new Go error with no PAM type.
 func (m *moduleWrapper) SimulateClientError(errorMsg string) error {
 	return errors.New(errorMsg)
+}
+
+// SimulateClientSignal sends a signal to the child process.
+func (m *moduleWrapper) SimulateClientSignal(sig syscall.Signal) {
+	pid := os.Getpid()
+	log.Debugf(context.Background(), "Sending signal %v to self pid (%v)",
+		sig, pid)
+
+	c := make(chan os.Signal, 1)
+	done := make(chan struct{})
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for s := range c {
+			if s != sig {
+				continue
+			}
+			log.Debugf(context.Background(), "Signal %v received", sig)
+			close(done)
+			return
+		}
+	}()
+
+	if err := syscall.Kill(pid, sig); err != nil {
+		log.Errorf(context.Background(), "Sending signal %v failed: %v", sig, err)
+		return
+	}
+	<-done
 }
