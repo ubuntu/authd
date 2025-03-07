de292eec5112f5b6d8b4c51a15f4f0c525701316
Marco Trevisan (Treviño) <mail@3v1n0.net>
2025-03-07 05:05:11 +0100

    pam/gdm/extension: Optimize the static values of the PAM requests
    We're sending thousands of requests, all with the same header content
    but we were using Go to init them which isn't great due to the fact that
    we end up having to allocate and free the same string maaaany times for
    no reason, while we can just use a defined value that gcc will nicely
    optimize in various places
    
    So, add few optimized functions and use them to init the protocol in the
    main cases, while we still keep a full initializer for testing purposes

--- pam/internal/gdm/extension.h
+++ pam/internal/gdm/extension.h
@@ -6,6 +6,9 @@
 // FIXME: Use system one once possible.
 #include "extensions/gdm-custom-json-pam-extension.h"
 
+#define JSON_PROTO_NAME ((const char *) "com.ubuntu.authd.gdm")
+#define JSON_PROTO_VERSION 1U
+
 static char pam_extension_environment_block[_POSIX_ARG_MAX];
 static char **supported_extensions = NULL;
 
@@ -45,4 +48,21 @@ gdm_custom_json_request_init (GdmPamExtensionJSONProtocol *request,
 {
   GDM_PAM_EXTENSION_CUSTOM_JSON_REQUEST_INIT (request, proto_name,
                                               proto_version, json);
+}
+
+static inline void
+gdm_custom_json_request_init_authd (GdmPamExtensionJSONProtocol *request,
+                                    const char                  *json)
+{
+  GDM_PAM_EXTENSION_CUSTOM_JSON_REQUEST_INIT (request, JSON_PROTO_NAME,
+                                              JSON_PROTO_VERSION, json);
+}
+
+static inline bool
+gdm_custom_json_request_is_valid_authd (GdmPamExtensionJSONProtocol *request)
+{
+  if (request->version != JSON_PROTO_VERSION)
+    return false;
+
+  return strncmp (request->protocol_name, JSON_PROTO_NAME, sizeof (request->protocol_name)) == 0;
 }
