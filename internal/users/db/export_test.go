66b3387ee17e9d6eb28c727bafa5c6f7baa30f8f
Adrian Dombeck <adrian.dombeck@canonical.com>
2025-03-18 16:11:01 +0100

    Also rename user in /etc/group file

--- internal/users/db/export_test.go
+++ internal/users/db/export_test.go
@@ -5,6 +5,16 @@ func (m *Manager) Path() string {
 	return m.path
 }
 
+// UserGroupFile exposes the path to the user group file for testing.
+func UserGroupFile() string {
+	return userGroupFile
+}
+
+// SetUserGroupFile sets the path to the user group file for testing.
+func SetUserGroupFile(path string) {
+	userGroupFile = path
+}
+
 // GetCreateSchemaQuery exposes the query to create the schema for testing.
 func GetCreateSchemaQuery() string {
 	return createSchemaQuery
