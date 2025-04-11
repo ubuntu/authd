e7dbf4bd546a6b00b45cc9394625bc854ab10bd3
Adrian Dombeck <adrian.dombeck@canonical.com>
2025-04-11 21:21:46 +0200

    Revert "Replace gpasswd testutil with TestHelperProcess"
    It complicated things when trying to run the helper with sudo, and I
    couldn't get the helper process to print it's output for debugging
    purposes (passing `-test.v` didn't work, I don't know why).
    
    This reverts commit 9801fdee6075c566e33b2f39e8082b4ca6fbe886.

--- internal/userutils/userutils_test.go
+++ internal/userutils/userutils_test.go
@@ -4,21 +4,28 @@ import (
 	"context"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"strings"
-	"syscall"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"github.com/ubuntu/authd/internal/userutils"
-	"github.com/ubuntu/authd/log"
 )
 
+//go:generate go build -o testhelpers/gpasswd ./testhelpers/gpasswd.go
+
 func TestLockAndUnlockGroupFile(t *testing.T) {
+	// Ensure the helper binary is built before running the test
+	cmd := exec.Command("go", "generate")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	require.NoError(t, err)
+
 	// Create a temporary group file
 	tempGroupFile := t.TempDir() + "/group"
+	require.NoError(t, err)
+	// Create the temporary group file
 	//nolint:gosec // G306 The group file is expected to have permissions 0644
-	err := os.WriteFile(tempGroupFile, []byte("testgroup:x:1001:testuser"), 0644)
+	err = os.WriteFile(tempGroupFile, []byte("testgroup:x:1001:testuser"), 0644)
 	require.NoError(t, err)
 
 	// Set the group file to the temporary file
@@ -26,7 +33,7 @@ func TestLockAndUnlockGroupFile(t *testing.T) {
 
 	// Try using gpasswd to modify the group file. This should succeed, because
 	// the group file is not locked.
-	output, err := runGPasswdHelper(t, tempGroupFile, "--add", "root", "testgroup")
+	output, err := runGPasswd(tempGroupFile, "--add", "root", "testgroup")
 	require.NoError(t, err, string(output))
 
 	// Lock the group file
@@ -35,7 +42,7 @@ func TestLockAndUnlockGroupFile(t *testing.T) {
 
 	// Try using gpasswd to modify the group file. This should fail, because
 	// the group file is locked.
-	output, err = runGPasswdHelper(t, tempGroupFile, "--delete", "root", "testgroup")
+	output, err = runGPasswd(tempGroupFile, "--delete", "root", "testgroup")
 	require.Error(t, err, string(output))
 	require.Contains(t, string(output), "gpasswd: cannot lock /etc/group")
 
@@ -49,71 +56,12 @@ func TestLockAndUnlockGroupFile(t *testing.T) {
 
 	// Try using gpasswd to modify the group file again. This should succeed,
 	// because the group file is unlocked.
-	output, err = runGPasswdHelper(t, tempGroupFile, "--delete", "root", "testgroup")
+	output, err = runGPasswd(tempGroupFile, "--delete", "root", "testgroup")
 	require.NoError(t, err, string(output))
 }
 
-func runGPasswdHelper(t *testing.T, groupFile string, args ...string) ([]byte, error) {
-	t.Helper()
-
-	args = append([]string{"-test.run=TestHelperProcess", "--", "gpasswd", groupFile}, args...)
-	// #nosec:G204 it's fine that we allow passing arbitrary arguments to gpasswd
-	cmd := exec.Command(os.Args[0], args...)
-	cmd.Env = append(os.Environ(), "GO_WANT_HELPER_PROCESS=1")
+func runGPasswd(groupFile string, args ...string) ([]byte, error) {
+	cmd := exec.Command("testhelpers/gpasswd", args...)
+	cmd.Env = append(os.Environ(), "GROUP_FILE="+groupFile)
 	return cmd.CombinedOutput()
-}
-
-func TestHelperProcess(t *testing.T) {
-	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
-		return
-	}
-
-	// Get the arguments after the '--'
-	args := os.Args
-	for len(args) > 0 {
-		if args[0] != "--" {
-			args = args[1:]
-			continue
-		}
-		args = args[1:]
-		break
-	}
-
-	cmd, args := args[0], args[1:]
-	switch cmd {
-	case "gpasswd":
-		if len(args) == 0 {
-			t.Fatal("gpasswd command requires at least one argument")
-		}
-		groupFile, args := args[0], args[1:]
-		execGPasswd(t, groupFile, args...)
-	default:
-		t.Fatalf("unknown command: %s", cmd)
-	}
-}
-
-func execGPasswd(t *testing.T, groupFile string, args ...string) {
-	t.Helper()
-
-	if filepath.Base(groupFile) != "group" {
-		t.Fatalf("the group file must be named 'group'.")
-	}
-
-	bwrapPath, err := exec.LookPath("bwrap")
-	require.NoError(t, err)
-
-	args = append([]string{
-		bwrapPath,
-		"--unshare-user",
-		"--uid", "0",
-		"--ro-bind", "/", "/",
-		"--bind", filepath.Dir(groupFile), "/etc",
-		"--ro-bind", "/etc/passwd", "/etc/passwd",
-		"gpasswd",
-	}, args...)
-
-	log.Infof(context.Background(), "Executing command: %s", strings.Join(args, " "))
-	//nolint:gosec // G204 there is no security issue with the arguments passed to syscall.Exec
-	err = syscall.Exec(args[0], args, os.Environ())
-	require.NoError(t, err)
 }
