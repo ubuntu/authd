e7201267ca539b0f2643a643e3b33e81d893a08a
Ubuntu Enterprise Desktop <ubuntu-devel-discuss@lists.ubuntu.com>
2025-05-14 08:17:11 +0000

    Update ubuntu noble package sources
    Use upstream commit d2d8b03bd3a890737aba00c8a8b24e85059ac70f

--- pam/internal/adapter/nativemodel.go
+++ pam/internal/adapter/nativemodel.go
@@ -204,15 +204,17 @@ func (m nativeModel) Update(msg tea.Msg) (nativeModel, tea.Cmd) {
 		return m.startAsyncOp(m.userSelection)
 
 	case brokersListReceived:
-		if m.serviceName == polkitServiceName {
+		m.availableBrokers = msg.brokers
+
+		// We should only handle this special case if there's more than one broker available.
+		// Otherwise, we will break polkit for local users.
+		if m.serviceName == polkitServiceName && len(msg.brokers) > 1 {
 			// Do not support using local broker in the polkit case.
 			// FIXME: This should be up to authd to keep a list of brokers based on service.
-			m.availableBrokers = slices.DeleteFunc(msg.brokers, func(b *authd.ABResponse_BrokerInfo) bool {
+			m.availableBrokers = slices.DeleteFunc(slices.Clone(m.availableBrokers), func(b *authd.ABResponse_BrokerInfo) bool {
 				return b.Id == brokers.LocalBrokerName
 			})
-			return m, nil
 		}
-		m.availableBrokers = msg.brokers
 
 	case authModesReceived:
 		m.authModes = msg.authModes
