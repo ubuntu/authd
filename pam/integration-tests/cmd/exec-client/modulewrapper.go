fc017938944072f74adae9d2af830d05731e6e85
Marco Trevisan (Treviño) <mail@3v1n0.net>
2025-05-22 02:09:00 +0200

    pam/dbusmodule: Do not use a pointer receiver for transaction
    We don't need to modify it, so it can just be a normal struct

--- pam/integration-tests/cmd/exec-client/modulewrapper.go
+++ pam/integration-tests/cmd/exec-client/modulewrapper.go
@@ -18,35 +18,35 @@ import (
 )
 
 type moduleWrapper struct {
-	*dbusmodule.Transaction
+	dbusmodule.Transaction
 }
 
 // Statically Ensure that [moduleWrapper] implements [pam.ModuleTransaction].
 var _ pam.ModuleTransaction = &moduleWrapper{}
 
-func newModuleWrapper(serverAddress string) (*moduleWrapper, func(), error) {
+func newModuleWrapper(serverAddress string) (moduleWrapper, func(), error) {
 	mTx, closeFunc, err := dbusmodule.NewTransaction(context.TODO(), serverAddress)
-	return &moduleWrapper{mTx}, closeFunc, err
+	return moduleWrapper{mTx}, closeFunc, err
 }
 
 // SimulateClientPanic forces the client to panic with the provided text.
-func (m *moduleWrapper) CallUnhandledMethod() error {
+func (m moduleWrapper) CallUnhandledMethod() error {
 	method := "com.ubuntu.authd.pam.UnhandledMethod"
 	return m.BusObject().Call(method, dbus.FlagNoAutoStart).Err
 }
 
 // SimulateClientPanic forces the client to panic with the provided text.
-func (m *moduleWrapper) SimulateClientPanic(text string) {
+func (m moduleWrapper) SimulateClientPanic(text string) {
 	panic(text)
 }
 
 // SimulateClientError forces the client to return a new Go error with no PAM type.
-func (m *moduleWrapper) SimulateClientError(errorMsg string) error {
+func (m moduleWrapper) SimulateClientError(errorMsg string) error {
 	return errors.New(errorMsg)
 }
 
 // SimulateClientSignal sends a signal to the child process.
-func (m *moduleWrapper) SimulateClientSignal(sig syscall.Signal, shouldExit bool) {
+func (m moduleWrapper) SimulateClientSignal(sig syscall.Signal, shouldExit bool) {
 	pid := os.Getpid()
 	log.Debugf(context.Background(), "Sending signal %v to self pid (%v)",
 		sig, pid)
