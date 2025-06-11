cb03d5055dcdf5c9c284a1cbd19fe2a5d27a64e1
Marco Trevisan (Treviño) <mail@3v1n0.net>
2025-06-11 17:56:35 +0200

    pam/integration-tests: Check the behavior of Authd with no brokers
    Allow to run authd without any broker, and just run the tests to ensure
    that we don't break this basic experience for various setups and
    special cases

--- pam/integration-tests/ssh_test.go
+++ pam/integration-tests/ssh_test.go
@@ -139,14 +139,15 @@ func testSSHAuthenticate(t *testing.T, sharedSSHd bool) {
 		tapeSettings  []tapeSetting
 		tapeVariables map[string]string
 
-		user             string
-		isLocalUser      bool
-		userPrefix       string
-		pamServiceName   string
-		socketPath       string
-		daemonizeSSHd    bool
-		interactiveShell bool
-		oldDB            string
+		user               string
+		isLocalUser        bool
+		userPrefix         string
+		pamServiceName     string
+		socketPath         string
+		daemonizeSSHd      bool
+		interactiveShell   bool
+		noConfiguredBroker bool
+		oldDB              string
 
 		wantUserAlreadyExist bool
 		wantNotLoggedInUser  bool
@@ -335,6 +336,14 @@ Wait@%dms`, sshDefaultFinalWaitTimeout),
 				vhsCommandFinalAuthWaitVariable: `Wait /Password:/`,
 			},
 		},
+		"Exit_authd_if_no_broker_is_configured_with_preset_user": {
+			tape:                "local_user_preset",
+			noConfiguredBroker:  true,
+			wantNotLoggedInUser: true,
+			tapeVariables: map[string]string{
+				vhsCommandFinalAuthWaitVariable: `Wait /Password:/`,
+			},
+		},
 		"Exit_authd_if_user_sigints": {
 			tape:                "sigint",
 			wantNotLoggedInUser: true,
@@ -377,13 +386,18 @@ Wait@%dms`, sshDefaultFinalWaitTimeout),
 			}
 
 			var groupsFile string
-			if tc.wantLocalGroups || tc.oldDB != "" {
+			needsLocalTestDaemonInstance := tc.wantLocalGroups || tc.noConfiguredBroker || tc.oldDB != ""
+
+			if needsLocalTestDaemonInstance {
 				// For the local groups tests we need to run authd again so that it has
 				// special environment that generates a fake gpasswd output for us to test.
 				// In the other cases this is not needed, so we can just use a shared authd.
 				gpasswdOutput, groupsFile = prepareGPasswdFiles(t)
 
 				authdEnv = append(authdEnv, useOldDatabaseEnv(t, tc.oldDB)...)
+				if tc.noConfiguredBroker {
+					authdEnv = append(authdEnv, "AUTHD_EXAMPLE_BROKER_DISABLE=1")
+				}
 
 				socketPath = runAuthd(t, gpasswdOutput, groupsFile, true,
 					testutils.WithEnvironment(authdEnv...))
@@ -428,7 +442,7 @@ Wait@%dms`, sshDefaultFinalWaitTimeout),
 
 			sshdPort := defaultSSHDPort
 			userHome := defaultUserHome
-			if !sharedSSHd || tc.wantLocalGroups || tc.oldDB != "" ||
+			if !sharedSSHd || needsLocalTestDaemonInstance ||
 				tc.interactiveShell || tc.socketPath != "" {
 				sshdEnv := sshdEnv
 				if nssLibrary != "" {
