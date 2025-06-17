c50b72c661acd8d1cc455d56ce48956e14ef5996
Adrian Dombeck <adrian.dombeck@canonical.com>
2025-06-17 12:29:35 +0200

    Add `authctl user set-uid` command

---
+++ cmd/authctl/user/set-uid.go
@@ -0,0 +1,46 @@
+package user
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strconv"
+
+	"github.com/spf13/cobra"
+	"github.com/ubuntu/authd/internal/proto/authd"
+)
+
+// setUIDCmd is a command to set the UID of a user managed by authd.
+var setUIDCmd = &cobra.Command{
+	Use:   "set-uid <name> <uid>",
+	Short: "Set the UID of a user managed by authd",
+	Args:  cobra.ExactArgs(2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		name := args[0]
+		uidStr := args[1]
+		uid, err := strconv.ParseUint(uidStr, 10, 32)
+		if err != nil {
+			// Remove the "strconv.ParseUint: parsing ..." part from the error message
+			// because it doesn't add any useful information.
+			if unwrappedErr := errors.Unwrap(err); unwrappedErr != nil {
+				err = unwrappedErr
+			}
+			return fmt.Errorf("failed to parse UID %q: %w", uidStr, err)
+		}
+
+		client, err := NewUserServiceClient()
+		if err != nil {
+			return err
+		}
+
+		_, err = client.SetUserID(context.Background(), &authd.SetUserIDRequest{
+			Name: name,
+			Id:   uint32(uid),
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
