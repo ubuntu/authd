c26b335d4a181f9bdd7f97ca92675f83817f0e83
Adrian Dombeck <adrian.dombeck@canonical.com>
2025-06-17 13:38:01 +0200

    Return a gRPC error with code PermissionDenied
    ... instead of prefixing the error message with "permission denied"

--- internal/services/permissions.go
+++ internal/services/permissions.go
@@ -5,16 +5,18 @@ import (
 	"strings"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (m Manager) globalPermissions(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	if strings.HasPrefix(info.FullMethod, "/authd.PAM/") {
 		if err := m.pamService.CheckGlobalAccess(ctx, info.FullMethod); err != nil {
-			return nil, err
+			return nil, status.Error(codes.PermissionDenied, err.Error())
 		}
 	} else if strings.HasPrefix(info.FullMethod, "/authd.NSS/") {
 		if err := m.userService.CheckGlobalAccess(ctx, info.FullMethod); err != nil {
-			return nil, err
+			return nil, status.Error(codes.PermissionDenied, err.Error())
 		}
 	}
 
