ed8068753d8f46ca8390eb3d418d719c68df4ef0
Adrian Dombeck <adrian.dombeck@canonical.com>
2025-06-20 13:44:09 +0200

    authctl: Add tests for root command

---
+++ cmd/authctl/main_test.go
@@ -0,0 +1,68 @@
+package main_test
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/ubuntu/authd/internal/testutils"
+	"github.com/ubuntu/authd/internal/testutils/golden"
+)
+
+var authctlPath string
+
+func TestRootCommand(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		args             []string
+		expectedExitCode int
+	}{
+		"Usage_message_when_no_args": {expectedExitCode: 0},
+		"Help_command":               {args: []string{"help"}, expectedExitCode: 0},
+		"Help_flag":                  {args: []string{"--help"}, expectedExitCode: 0},
+		"Completion_command":         {args: []string{"completion"}, expectedExitCode: 0},
+
+		"Error_on_invalid_command": {args: []string{"invalid-command"}, expectedExitCode: 1},
+		"Error_on_invalid_flag":    {args: []string{"--invalid-flag"}, expectedExitCode: 1},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			//nolint:gosec // G204 it's safe to use exec.Command with a variable here
+			cmd := exec.Command(authctlPath, tc.args...)
+			t.Logf("Running command: %s", cmd.String())
+			outputBytes, err := cmd.CombinedOutput()
+			output := string(outputBytes)
+			exitCode := cmd.ProcessState.ExitCode()
+
+			if tc.expectedExitCode == 0 && err != nil {
+				t.Logf("Command output:\n%s", output)
+				t.Errorf("Expected no error, but got: %v", err)
+			}
+
+			if exitCode != tc.expectedExitCode {
+				t.Logf("Command output:\n%s", output)
+				t.Errorf("Expected exit code %d, got %d", tc.expectedExitCode, exitCode)
+			}
+
+			golden.CheckOrUpdate(t, output)
+		})
+	}
+}
+
+func TestMain(m *testing.M) {
+	var cleanup func()
+	var err error
+	authctlPath, cleanup, err = testutils.BuildAuthctl()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Setup: %v\n", err)
+		os.Exit(1)
+	}
+	defer cleanup()
+
+	m.Run()
+}
