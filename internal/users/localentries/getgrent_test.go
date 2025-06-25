ae4cc09a3bdae52caca74e9c875a5cb763e7c87f
Ubuntu Enterprise Desktop <ubuntu-devel-discuss@lists.ubuntu.com>
2025-06-25 17:20:34 +0000

    Update ubuntu devel package sources
    Use upstream commit 57eb1aba68e317913db041378465c20b7305c17d

--- internal/users/localentries/getgrent_test.go
+++ internal/users/localentries/getgrent_test.go
@@ -1,6 +1,8 @@
 package localentries
 
 import (
+	"fmt"
+	"slices"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,24 +11,47 @@ import (
 func TestGetGroupEntries(t *testing.T) {
 	t.Parallel()
 
-	got, err := GetGroupEntries()
-	require.NoError(t, err, "GetGroupEntries should never return an error")
-	require.NotEmpty(t, got, "GetGroupEntries should never return an empty list")
+	// Ensure the requests can be parallelized
+	for idx := range 10 {
+		t.Run(fmt.Sprintf("iteration_%d", idx), func(t *testing.T) {
+			t.Parallel()
+
+			got, err := GetGroupEntries()
+			require.NoError(t, err, "GetGroupEntries should never return an error")
+			require.NotEmpty(t, got, "GetGroupEntries should never return an empty list")
+			require.True(t, slices.ContainsFunc(got, func(g Group) bool {
+				return g.Name == "root" && g.GID == 0
+			}), "GetGroupEntries should return root")
+		})
+	}
 }
 
 func TestGetGroupByName(t *testing.T) {
 	t.Parallel()
 
-	got, err := GetGroupByName("root")
-	require.NoError(t, err, "GetGroupByName should not return an error")
-	require.Equal(t, got.Name, "root")
-	require.Equal(t, got.GID, uint32(0))
+	for idx := range 10 {
+		t.Run(fmt.Sprintf("iteration_%d", idx), func(t *testing.T) {
+			t.Parallel()
+
+			got, err := GetGroupByName("root")
+			require.NoError(t, err, "GetGroupByName should not return an error")
+			require.Equal(t, got.Name, "root")
+			require.Equal(t, got.GID, uint32(0))
+			require.Equal(t, got.Passwd, "x")
+		})
+	}
 }
 
 func TestGetGroupByName_NotFound(t *testing.T) {
 	t.Parallel()
 
-	got, err := GetGroupByName("nonexistent")
-	require.ErrorIs(t, err, ErrGroupNotFound)
-	require.Equal(t, got.Name, "")
+	for idx := range 10 {
+		t.Run(fmt.Sprintf("iteration_%d", idx), func(t *testing.T) {
+			t.Parallel()
+
+			got, err := GetGroupByName(fmt.Sprintf("nonexistent-really-%d", idx))
+			require.ErrorIs(t, err, ErrGroupNotFound)
+			require.Equal(t, got.Name, "")
+		})
+	}
 }
--- internal/users/localentries/getpwent_test.go
+++ internal/users/localentries/getpwent_test.go
@@ -1,6 +1,7 @@
 package localentries
 
 import (
+	"fmt"
 	"slices"
 	"testing"
 
@@ -10,30 +11,49 @@ import (
 func TestGetPasswdEntries(t *testing.T) {
 	t.Parallel()
 
-	got, err := GetPasswdEntries()
-	require.NoError(t, err, "GetPasswdEntries should never return an error")
-	require.NotEmpty(t, got, "GetPasswdEntries should never return an empty list")
+	for idx := range 10 {
+		t.Run(fmt.Sprintf("iteration_%d", idx), func(t *testing.T) {
+			t.Parallel()
 
-	// Check if the root user is present in the list
-	rootFound := slices.ContainsFunc(got, func(entry Passwd) bool {
-		return entry.Name == "root"
-	})
-	require.True(t, rootFound, "GetPasswdEntries should always return root")
+			got, err := GetPasswdEntries()
+			require.NoError(t, err, "GetPasswdEntries should never return an error")
+			require.NotEmpty(t, got, "GetPasswdEntries should never return an empty list")
+
+			// Check if the root user is present in the list
+			rootFound := slices.ContainsFunc(got, func(entry Passwd) bool {
+				return entry.Name == "root" && entry.UID == 0
+			})
+			require.True(t, rootFound, "GetPasswdEntries should always return root")
+		})
+	}
 }
 
 func TestGetPasswdByName(t *testing.T) {
 	t.Parallel()
 
-	got, err := GetPasswdByName("root")
-	require.NoError(t, err, "GetPasswdByName should not return an error")
-	require.Equal(t, got.Name, "root")
-	require.Equal(t, got.UID, uint32(0))
+	for idx := range 10 {
+		t.Run(fmt.Sprintf("iteration_%d", idx), func(t *testing.T) {
+			t.Parallel()
+
+			got, err := GetPasswdByName("root")
+			require.NoError(t, err, "GetPasswdByName should not return an error")
+			require.Equal(t, "root", got.Name, "Name does not match")
+			require.Equal(t, uint32(0), got.UID, "UID does not match")
+			require.Equal(t, "root", got.Gecos, "Gecos does not match")
+		})
+	}
 }
 
 func TestGetPasswdByName_NotFound(t *testing.T) {
 	t.Parallel()
 
-	got, err := GetPasswdByName("nonexistent")
-	require.ErrorIs(t, err, ErrUserNotFound)
-	require.Equal(t, got.Name, "")
+	for idx := range 10 {
+		t.Run(fmt.Sprintf("iteration_%d", idx), func(t *testing.T) {
+			t.Parallel()
+
+			got, err := GetPasswdByName(fmt.Sprintf("nonexistent-really-%d", idx))
+			require.ErrorIs(t, err, ErrUserNotFound)
+			require.Empty(t, got, "Entry should be empty, but is not")
+		})
+	}
 }
