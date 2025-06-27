a13699fe7ff206f96043a374ea60f2cfaade4df8
Ubuntu Enterprise Desktop <ubuntu-devel-discuss@lists.ubuntu.com>
2025-06-27 14:25:57 +0000

    Update ubuntu plucky package sources
    Use upstream commit 1bd5857b3097f71cf924c1f8dc5e9e1ad821a1f3

--- internal/services/pam/permissions.go
+++ internal/services/pam/permissions.go
@@ -4,5 +4,5 @@ import "context"
 
 // CheckGlobalAccess denies all requests not coming from the root user.
 func (s Service) CheckGlobalAccess(ctx context.Context, method string) error {
-	return s.permissionManager.IsRequestFromRoot(ctx)
+	return s.permissionManager.CheckRequestIsFromRoot(ctx)
 }
