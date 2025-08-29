e3d809af487984a73039626afb5b92bc9d47f46e
Ubuntu Enterprise Desktop <ubuntu-devel-discuss@lists.ubuntu.com>
2025-08-29 08:31:18 +0000

    Update ubuntu plucky package sources
    Use upstream commit 736d6e86c2a6205d9e68d48b9021b88857da87bd

--- nss/src/logs/mod.rs
+++ nss/src/logs/mod.rs
@@ -1,13 +1,15 @@
 use log::{LevelFilter, Metadata};
+use once_cell::sync::OnceCell;
 use simple_logger::SimpleLogger;
 use std::env;
 use syslog::{BasicLogger, Facility, Formatter3164};
 
+pub static LOG_PREFIX: OnceCell<&'static str> = OnceCell::new();
+
 #[macro_export]
 macro_rules! info {
     ($($arg:tt)*) => {
-        let log_prefix = "authd:";
-        log::info!("{} {}", log_prefix, format_args!($($arg)*));
+        log::info!("{}{}", $crate::logs::LOG_PREFIX.get().copied().unwrap_or(""), format_args!($($arg)*));
     }
 }
 
@@ -36,33 +38,48 @@ pub fn init_logger() {
 
 /// init_sys_logger initializes a global log that prints messages to the system logs.
 fn init_sys_logger(log_level: LevelFilter) {
+    // Derive the process name from current_exe(), fall back to a sensible default.
+    let process_name = std::env::current_exe()
+        .ok()
+        .and_then(|p| p.file_name().map(|s| s.to_string_lossy().into_owned()))
+        .filter(|s| !s.is_empty())
+        .unwrap_or_else(|| "nss-authd".to_string());
+
     let formatter = Formatter3164 {
         facility: Facility::LOG_USER,
         hostname: None,
-        process: "authd".into(),
-        pid: 0,
+        process: process_name,
+        pid: std::process::id(),
     };
 
-    let logger = match syslog::unix(formatter) {
-        Err(err) => {
-            println!("cannot connect to syslog: {err:?}");
-            return;
-        }
-        Ok(l) => l,
-    };
-
-    if let Err(err) = log::set_boxed_logger(Box::new(BasicLogger::new(logger)))
-        .map(|()| log::set_max_level(log_level))
-    {
-        eprintln!("cannot set log level: {err:?}");
+    let logger = if let Ok(l) = syslog::unix(formatter) {
+        l
+    } else {
+        eprintln!("failed to create syslog logger");
         return;
     };
+
+    if let Err(err) = log::set_boxed_logger(Box::new(BasicLogger::new(logger))) {
+        eprintln!("failed to install global syslog logger: {err:?}");
+        return;
+    }
+    log::set_max_level(log_level);
+
+    LOG_PREFIX.set("[nss-authd] ").unwrap();
 
     info!("Log output set to syslog");
 }
 
 /// init_stderr_logger initializes a global log that prints the messages to stderr.
 fn init_stderr_logger(log_level: LevelFilter) {
-    SimpleLogger::new().with_level(log_level).init().unwrap();
+    SimpleLogger::new()
+        .with_level(log_level)
+        .with_local_timestamps()
+        .with_timestamp_format(time::macros::format_description!(
+            "[hour]:[minute]:[second]"
+        ))
+        .init()
+        .unwrap();
+
     info!("Log output set to stderr");
 }
