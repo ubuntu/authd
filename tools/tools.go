cc3da20bdd4762628ef07bf4524bd2e140fbf07f
Adrian Dombeck <adrian.dombeck@canonical.com>
2025-08-31 11:57:31 +0200

    Update golangci-lint to v2 and migrate its config
    This allows us to use golangci-lint with Go 1.25.
    
    Config was migrated via `golangci-lint migrate` and manually adding back
    the comments.

--- tools/tools.go
+++ tools/tools.go
@@ -5,7 +5,7 @@
 package tools
 
 import (
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/golangci/golangci-lint/v2/cmd/golangci-lint"
 	_ "github.com/msteinert/pam/v2/cmd/pam-moduler"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "github.com/golang/protobuf/protoc-gen-go"
