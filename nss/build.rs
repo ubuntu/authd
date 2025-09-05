d025cb23f08a0e0489a48c83c34bd0ae451ad221
Adrian Dombeck <adrian.dombeck@canonical.com>
2025-09-05 12:04:34 +0200

    Migrate to tonic 0.14
    It just required using tonic-prost-build instead of tonic-build.

--- nss/build.rs
+++ nss/build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .build_server(false)
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(
