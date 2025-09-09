d44d4519547812d2e63ee4ce823f83788c3d1006
Adrian Dombeck <adrian.dombeck@canonical.com>
2025-09-09 12:19:23 +0200

    ci: Use the dpkg-install-speedup GitHub Action
    And move the `uses: actions/checkout@v5` line back to where it was
    before 58f21eb16604888c17764afecae7e4ffed7b84e6.

--- .github/scripts/dpkg-exclude-locales-and-manpages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cat <<EOF | sudo tee /etc/dpkg/dpkg.cfg.d/01_nodoc
-path-exclude=/usr/share/locale/*
-path-exclude=/usr/share/man/*
-path-exclude=/usr/share/doc/*
-path-exclude=/usr/share/info/*
-EOF
