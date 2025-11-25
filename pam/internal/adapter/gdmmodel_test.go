0cf8b71b59a1e6499541b0c8ab9e6e2284c2dce3
D.S <dima@layupparts.com>
2025-11-24 18:26:45 -0800

    Add optional require_2fa argument
    Downstream PAM modules can branch off cred_insufficient to require a second auth factor

--- pam/internal/adapter/gdmmodel_test.go
+++ pam/internal/adapter/gdmmodel_test.go
@@ -2475,7 +2475,7 @@ func TestGdmModel(t *testing.T) {
 
 			var exitStatus PamReturnStatus
 			uiModel := newUIModelForClients(pam_test.NewModuleTransactionDummy(gdmHandler),
-				Gdm, tc.sessionMode, tc.client, nil, &exitStatus)
+				Gdm, tc.sessionMode, false, tc.client, nil, &exitStatus)
 
 			appState := gdmTestUIModel{
 				uiModel:             uiModel,
