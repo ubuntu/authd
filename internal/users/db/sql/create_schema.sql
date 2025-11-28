623802d9a89af8fc5f70ab5e9adb369ba3ff4a68
Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>
2025-11-28 10:01:34 +0100

    fix: table names must be lowercase

--- internal/users/db/sql/create_schema.sql
+++ internal/users/db/sql/create_schema.sql
@@ -10,20 +10,20 @@ CREATE TABLE IF NOT EXISTS users (
 );
 CREATE UNIQUE INDEX "idx_user_name" ON users ("name");
 
-CREATE TABLE IF NOT EXISTS GROUPS (
+CREATE TABLE IF NOT EXISTS groups (
     name TEXT NOT NULL,  -- Uniqueness is enforced by the index below
     gid  INT PRIMARY KEY, -- Uniqueness and not NULL is enforced by PRIMARY KEY
     ugid TEXT NOT NULL    -- Uniqueness is enforced by the index below
 );
-CREATE UNIQUE INDEX "idx_group_name" ON GROUPS ("name");
-CREATE UNIQUE INDEX "idx_group_ugid" ON GROUPS ("ugid");
+CREATE UNIQUE INDEX "idx_group_name" ON groups ("name");
+CREATE UNIQUE INDEX "idx_group_ugid" ON groups ("ugid");
 
 CREATE TABLE IF NOT EXISTS users_to_groups (
     uid INT NOT NULL,
     gid INT NOT NULL,
     PRIMARY KEY (uid, gid),
     FOREIGN KEY (uid) REFERENCES users (uid) ON DELETE CASCADE,
-    FOREIGN KEY (gid) REFERENCES GROUPS (gid) ON DELETE CASCADE
+    FOREIGN KEY (gid) REFERENCES groups (gid) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS users_to_local_groups (
