0b9d8c062a803290d772c6f168717781a7080422
Adrian Dombeck <adrian.dombeck@canonical.com>
2025-12-02 15:28:05 +0100

    Add `authctl group` command

---
+++ cmd/authctl/group/group.go
@@ -0,0 +1,14 @@
+// Package group provides utilities for managing group operations.
+package group
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// GroupCmd is a command to perform group-related operations.
+var GroupCmd = &cobra.Command{
+	Use:   "group",
+	Short: "Commands related to groups",
+	Args:  cobra.NoArgs,
+	RunE:  func(cmd *cobra.Command, args []string) error { return cmd.Usage() },
+}
