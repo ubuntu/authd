b63bd81d2a548bd8ffa5cf0a77e9f17784e300ba
Adrian Dombeck <adrian.dombeck@canonical.com>
2025-12-03 14:40:50 +0100

    authctl: Add long description for set-gid command

--- cmd/authctl/group/set-gid.go
+++ cmd/authctl/group/set-gid.go
@@ -16,7 +16,28 @@ import (
 var setGIDCmd = &cobra.Command{
 	Use:   "set-gid <name> <gid>",
 	Short: "Set the GID of a group managed by authd",
-	Args:  cobra.ExactArgs(2),
+	Long: `Set the GID of a group managed by authd to the specified value.
+
+The new GID value must be unique and non-negative.
+
+When a group's GID is changed, any users whose primary group is set to this group
+will have their primary group GID updated. The home directories of these users and
+files within them owned by the group will be updated to the new GID.
+
+Files outside users' home directories are not updated and must be changed
+manually. Note that changing a GID can be unsafe if files on the system are
+still owned by the original GID: those files may become accessible to a
+different group that is later assigned that GID. To change group ownership of
+all files on the system from the old GID to the new GID, run:
+
+  sudo chown -R --from :OLD_GID :NEW_GID /
+
+This command requires root privileges.
+
+Examples:
+  authctl group set-gid staff 30000
+  authctl group set-gid developers 40000`,
+	Args: cobra.ExactArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		name := args[0]
 		gidStr := args[1]
