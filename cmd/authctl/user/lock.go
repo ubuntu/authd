ff1e5b81af53750a0ba881c7b7e05b80d0d44cae
Adrian Dombeck <adrian.dombeck@canonical.com>
2026-01-06 01:51:27 +0100

    authctl: Add "long" descriptions for lock and unlock commands

--- cmd/authctl/user/lock.go
+++ cmd/authctl/user/lock.go
@@ -13,6 +13,7 @@ import (
 var lockCmd = &cobra.Command{
 	Use:               "lock <user>",
 	Short:             "Lock (disable) a user managed by authd",
+	Long:              `Lock a user so that it cannot log in.`,
 	Args:              cobra.ExactArgs(1),
 	ValidArgsFunction: completion.Users,
 	RunE: func(cmd *cobra.Command, args []string) error {
